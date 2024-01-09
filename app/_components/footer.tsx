e298bd030ca6f742c17563e3eaf0f37035e587d5
nicholavila <nicholavila1@outlook.com>
2024-01-09 14:49:37 +0000

    rearrange: add footer comonent

---
+++ app/_components/footer.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export const Footer = () => {
+  return (
+    <div className="w-full h-12 flex items-center justify-center bg-gray-200 z-10 relative">
+      <div className="w-5/6 flex justify-between">
+        <p>Copyright © 2024 MainManJS</p>
+        <div className="flex items-center gap-x-4 text-sm font-medium">
+          <Link href="/">
+            <p>Terms</p>
+          </Link>
+          <Link href="/">
+            <p>Privacy</p>
+          </Link>
+          <Link href="/">
+            <p>Support</p>
+          </Link>
+          <Link href="/">
+            <p>Sitemap</p>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
