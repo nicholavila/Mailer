247bbec88b49baa4bc2cb55aa6d7c5fcb37f41a4
nicholavila <nicholavila1@outlook.com>
2024-01-11 12:53:11 +0000

    update: update callback functions in global auth

--- auth.ts
+++ auth.ts
@@ -1,18 +1,43 @@
 import NextAuth from "next-auth";
 import authConfig from "@/auth.config";
+import adapter from "@/adapter";
 
-export const { handlers, auth } = NextAuth({
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter,
   session: { strategy: "jwt" },
+  // pages: {
+  //   signIn: "/auth/login",
+  //   error: "/auth/error"
+  // },
+  events: {
+    async linkAccount({ user }) {
+    }
+  },
   callbacks: {
     async signIn({ user, account }) {
-
+      console.log("__CALLBACK__signIn", user, account);
       return true;
     },
     async session({ token, session }) {
+      console.log("__CALLBACK__session", token, session);
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
+
+      if (session.user) {
+        session.user.name = token.name;
+        session.user.email = token.email as string;
+        session.user.isOAuth = token.isOAuth as boolean;
+      }
+
       return session;
     },
     async jwt({ token }) {
-
+      console.log("__CALLBACK__jwt", token);
       return token;
     }
   },
