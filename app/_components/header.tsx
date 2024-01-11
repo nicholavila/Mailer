1b2a5b3add80e184c4d89d5a5053e0250ff27850
nicholavila <nicholavila1@outlook.com>
2024-01-11 15:26:11 +0000

    update: update style of header

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -3,24 +3,21 @@
 import Link from "next/link";
 import React from "react";
 import { Navbar } from "./navbar";
+import { UserButton } from "@/components/auth/user-button";
 
 export const Header = () => {
   return (
-    <nav className="w-full pb-2 flex justify-center box-border border-b-[1px] z-10">
-      <div className="w-5/6 flex items-end justify-between">
+    <nav className="w-full flex justify-center box-border border-b-[1px] z-10">
+      <div className="w-5/6 flex items-center justify-between">
         <div className="flex">
           <Link href="/">
-            <p className="text-4xl font-semibold text-black drop-shadow-md">
+            <p className="text-4xl font-semibold text-black drop-shadow-md pb-4">
               MailManJS
             </p>
           </Link>
         </div>
         <Navbar />
-        <Link href="/auth/login">
-          <p className="text-xl font-semibold text-black drop-shadow-md">
-            Log In
-          </p>
-        </Link>
+        <UserButton />
       </div>
     </nav>
   );
