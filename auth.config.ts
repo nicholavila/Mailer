c2ebb95d05d7c21fb3b0fdfcccf1f141e0df424f
nicholavila <nicholavila1@outlook.com>
2024-01-11 11:48:06 +0000

    update: updat configuration of auth

--- auth.config.ts
+++ auth.config.ts
@@ -1,8 +1,31 @@
-// import bcrypt from "bcryptjs";
+import Credentials from "next-auth/providers/credentials";
+import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+import Discord from "next-auth/providers/discord";
+import Apple from "next-auth/providers/apple";
+import bcrypt from "bcryptjs";
 
 import type { NextAuthConfig } from "next-auth";
+import { getUserByEmail } from "@/data/user";
 
 export default {
   providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
+    }),
+    Github({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string
+    }),
+    Discord({
+      clientId: process.env.DISCORD_CLIENT_ID || "",
+      clientSecret: process.env.DISCORD_CLIENT_SECRET || ""
+    }),
+    Apple({
+      clientId: process.env.APPLE_CLIENT_ID || "",
+      clientSecret: process.env.APPLE_CLIENT_SECRET || ""
+    }),
+
   ]
 } satisfies NextAuthConfig;
