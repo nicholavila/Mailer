dc2b07ee88d57027b408e400ae0786e0ffc65ec8
nicholavila <nicholavila1@outlook.com>
2024-01-15 11:23:11 +0000

    feat: add reset server action

---
+++ actions/auth/reset.ts
@@ -0,0 +1,47 @@
+"use server";
+
+import * as z from "zod";
+import { v4 as uuidv4 } from "uuid";
+
+import { ResetSchema } from "@/schemas/auth";
+import { getUserByEmail, updateUserToken } from "@/data/user";
+import { sendPasswordResetEmail } from "@/lib/mail";
+import { generateVerificationToken } from "@/lib/tokens";
+
+export const reset = async (values: z.infer<typeof ResetSchema>) => {
+  const validatedFields = ResetSchema.safeParse(values);
+  if (!validatedFields.success) {
+    return { error: "Invalid email!" };
+  }
+
+  const { email } = validatedFields.data;
+
+  const existingUser = await getUserByEmail(email);
+  if (!existingUser) {
+    return { error: "Email not found!" };
+  }
+
+  const verificationToken = generateVerificationToken(email);
+
+  const updatedUser = await updateUserToken({
+    email,
+    verificationToken,
+    expires: new Date(new Date().getTime() + 3600 * 1000)
+  });
+
+  if (!updatedUser) {
+    return { error: "Server Error" };
+  }
+
+  // const passwordResetToken = await generatePasswordResetToken(email);
+  const response = await sendPasswordResetEmail(
+    updatedUser.email,
+    updatedUser.verificationToken
+  );
+
+  if (response.error) {
+    return { error: response.error.name };
+  }
+
+  return { success: "Reset email sent!" };
+};
