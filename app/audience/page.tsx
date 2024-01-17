d4e3b3d80976421ba69db641155d74dcb56d59a6
nicholavila <nicholavila1@outlook.com>
2024-01-17 12:40:59 +0000

    feat: add audience main logic

---
+++ app/audience/page.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import { Badge } from "@/components/ui/badge";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import AreaChartPlot from "@/components/utils/areachart-plot";
+import { FaArrowUp } from "react-icons/fa";
+
+const Blue = ({ children }: { children: React.ReactNode }) => {
+  return <span className="text-blue-700">{children}</span>;
+};
+
+const Strong = ({ children }: { children: React.ReactNode }) => {
+  return <span className="font-semibold">{children}</span>;
+};
+
+const AudiencePage = () => {
+  return (
+    <main className="w-5/6 flex flex-col py-6">
+      <p className="text-5xl text-green-700 font-semibold pb-6">
+        Audience Dashboard
+      </p>
+      <p className="text-2xl font-semibold pb-2">Hi, Malachi Uy!</p>
+      <p className="text-xl pb-6">
+        Your audience has{" "}
+        <Blue>
+          <Strong>17</Strong>{" "}
+        </Blue>{" "}
+        contacts.{" "}
+        <Blue>
+          <Strong>15</Strong>
+        </Blue>{" "}
+        of these are subscribed.
+      </p>
+
+      <div className="w-full flex gap-x-6 pb-6">
+        <Card className="w-[400px] flex items-center justify-between px-8 py-4">
+          <div className="flex flex-col gap-y-2">
+            <p className="text-xl font-semibold">Subscribers</p>
+            <p className="text-lg font-semibold">0</p>
+            <p className="text-base text-gray-700">from 0 (last 4 weeks)</p>
+          </div>
+          <Badge variant="default" className="flex gap-x-1 py-1 rounded-full">
+            <FaArrowUp /> 100%
+          </Badge>
+        </Card>
+        <Card className="w-[400px] flex items-center justify-between px-8 py-4">
+          <div className="flex flex-col gap-y-2">
+            <p className="text-xl font-semibold">Open Rate</p>
+            <p className="text-lg font-semibold">0</p>
+            <p className="text-base text-gray-700">from 0 (last 4 weeks)</p>
+          </div>
+          <Badge
+            variant="default"
+            className="flex gap-x-1 py-1 rounded-full bg-gray-700"
+          >
+            - 0%
+          </Badge>
+        </Card>
+        <Card className="w-[400px] flex items-center justify-between px-8 py-4">
+          <div className="flex flex-col gap-y-2">
+            <p className="text-xl font-semibold">Click Rate</p>
+            <p className="text-lg font-semibold">0</p>
+            <p className="text-base text-gray-700">from 0 (last 4 weeks)</p>
+          </div>
+          <Badge
+            variant="default"
+            className="flex gap-x-1 py-1 rounded-full bg-gray-700"
+          >
+            - 0%
+          </Badge>
+        </Card>
+      </div>
+
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-2xl">Recent Growth</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className="w-full h-[480px]">
+            <AreaChartPlot />
+          </div>
+        </CardContent>
+      </Card>
+    </main>
+  );
+};
+
+export default AudiencePage;
