52fc21b93e8e254511607dde1c3a2431cfb645a0
nicholavila <nicholavila1@outlook.com>
2024-01-17 10:55:32 +0000

    update: update navbar with live path

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -9,45 +9,92 @@ import {
   NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
+  navigationMenuTriggerStyle
 } from "@/components/ui/navigation-menu";
 import React from "react";
+import { ListItem } from "@/components/utils/list-item";
 
 type MenuItem = {
   title: string;
   href: string;
+  children?: { title: string; href: string; description: string }[];
 };
 
 export const Navbar = () => {
+  const audienceItems: { title: string; href: string; description: string }[] =
+    [
+      {
+        title: "Audience Dashboard",
+        href: "/audience",
+        description:
+          "Your audience dashboard is the home for all your contacts and audiences"
+      },
+      {
+        title: "All contacts",
+        href: "/audience/contacts",
+        description: "All your contacts stored in your customer database"
+      },
+      {
+        title: "Tags",
+        href: "/audience/tags",
+        description: "Group and manage your audience in tags"
+      },
+      {
+        title: "Segments",
+        href: "/audience/segments",
+        description: "Classify and manage your audience in segments"
+      }
+    ];
+
   const menuItems: MenuItem[] = [
     {
       title: "Features",
-      href: "/",
+      href: "/"
     },
     {
       title: "Pricing",
-      href: "/",
+      href: "/"
     },
     {
       title: "Resources",
-      href: "/",
+      href: "/"
     },
     {
       title: "Campaign",
-      href: "/",
+      href: "/"
     },
     {
-      title: "Customers",
-      href: "/",
-    },
+      title: "Audience",
+      href: "/audience",
+      children: audienceItems
+    }
   ];
+
   return (
     <NavigationMenu>
       <NavigationMenuList className="gap-x-2">
         {menuItems.map((menuItem, index) => (
           <NavigationMenuItem key={menuItem.title}>
-            <NavigationMenuTrigger>{menuItem.title}</NavigationMenuTrigger>
-            <NavigationMenuContent></NavigationMenuContent>
+            <NavigationMenuTrigger>
+              <Link href={menuItem.href} passHref>
+                {menuItem.title}
+              </Link>
+            </NavigationMenuTrigger>
+            {menuItem.children && (
+              <NavigationMenuContent>
+                <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                  {menuItem.children.map((item) => (
+                    <ListItem
+                      key={item.title}
+                      title={item.title}
+                      href={item.href}
+                    >
+                      {item.description}
+                    </ListItem>
+                  ))}
+                </ul>
+              </NavigationMenuContent>
+            )}
           </NavigationMenuItem>
         ))}
       </NavigationMenuList>
