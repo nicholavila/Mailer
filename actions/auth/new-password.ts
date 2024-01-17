49258c5958a470dc1e72d0f20ad9e1595020fdae
nicholavila <nicholavila1@outlook.com>
2024-01-17 08:28:57 +0000

    feat: add new-password feature

--- actions/auth/new-password.ts
+++ actions/auth/new-password.ts
@@ -50,29 +50,3 @@ export const newPassword = async (
 
   return { success: "Password updated!" };
 };
-
-// const existingToken = await getPasswordResetTokenByToken(token);
-// if (!existingToken) {
-//   return { error: "Invalid token!" };
-// }
-
-// const hasExpired = new Date(existingToken.expires) < new Date();
-// if (hasExpired) {
-//   return { error: "Token has expired!" };
-// }
-
-// const existingUser = await getUserByEmail(existingToken.email);
-// if (!existingUser) {
-//   return { error: "Email does not exist!" };
-// }
-
-// const hashedPassword = await bcrypt.hash(password, 10);
-
-// await db.user.update({
-//   where: { id: existingUser.id },
-//   data: { password: hashedPassword }
-// });
-
-// await db.passwordResetToken.delete({
-//   where: { id: existingToken.id }
-// });
