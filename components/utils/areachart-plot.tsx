2aadd2723785d8e870338333b84697ac3bb13978
nicholavila <nicholavila1@outlook.com>
2024-01-18 15:11:13 +0000

    update: update areachart setting

--- components/utils/areachart-plot.tsx
+++ components/utils/areachart-plot.tsx
@@ -10,39 +10,9 @@ import {
 const AreaChartPlot = () => {
   const data = [
     {
-      year: "2016",
-      Iphone: 4000,
-      Samsung: 2400
-    },
-    {
-      year: "2017",
-      Iphone: 3000,
-      Samsung: 1398
-    },
-    {
-      year: "2018",
-      Iphone: 2000,
-      Samsung: 9800
-    },
-    {
-      year: "2019",
-      Iphone: 2780,
-      Samsung: 3908
-    },
-    {
-      year: "2020",
-      Iphone: 1890,
-      Samsung: 4800
-    },
-    {
-      year: "2021",
-      Iphone: 2390,
-      Samsung: 3800
-    },
-    {
-      year: "2022",
-      Iphone: 3490,
-      Samsung: 4300
+      month: "2016",
+      sent: 4000,
+      received: 2400
     }
   ];
 
