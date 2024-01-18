47c497e2c9ebf9b3b7c2c4bf6580e1cd1ba7c40d
nicholavila <nicholavila1@outlook.com>
2024-01-18 13:53:14 +0000

    update: update from-file page of contacts

--- app/audience/contacts/from-file/page.tsx
+++ app/audience/contacts/from-file/page.tsx
@@ -9,25 +9,7 @@ const FromFile = () => {
     <main className="w-5/6 flex flex-col py-6">
       <p className="text-4xl font-semibold mb-6">Upload a File.</p>
       <p className="text-xl mb-8">Not sure how to format your file?</p>
-      <Link
-        href="/"
-        className="underline underline-offset-2 mb-2 text-blue-500"
-      >
-        Help me format my CSV
-      </Link>
-      <Link
-        href="/"
-        className="underline underline-offset-2 mb-2 text-blue-500 mb-8"
-      >
-        Watch a tutorial
-      </Link>
-      <Button
-        variant="outline"
-        className="w-96 h-32 flex gap-x-2 border-green-700 mb-8"
-      >
-        <FaFileUpload />
-        Browse
-      </Button>
+
       <Button asChild className="w-64 flex gap-x-2">
         <Link href={`/audience/contacts/add`}>
           <FaArrowLeft />
