55d81b33b63a2d5583c2817bde4d307658673691
nicholavila <nicholavila1@outlook.com>
2024-01-19 14:26:02 +0000

    update: update from-mannual page

--- app/audience/contacts/from-mannual/page.tsx
+++ app/audience/contacts/from-mannual/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 const FromMannual = () => {
-  return <main>From Mannual</main>;
+  return (
+    <main className="w-5/6 flex flex-col py-6">
+      <p className="text-4xl font-semibold mb-6">Mannual Input.</p>
+      <p className="text-xl mb-8">Not sure how to format your file?</p>
+    </main>
+  );
 };
 
 export default FromMannual;
