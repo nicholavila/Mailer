3c610170fff4bfc9d6a02ba11fcd6a71369ef9aa
nicholavila <nicholavila1@outlook.com>
2024-01-19 15:36:02 +0000

    update: add continue & back buttons into mannual page

--- app/audience/contacts/from-mannual/page.tsx
+++ app/audience/contacts/from-mannual/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 import Link from "next/link";
 import { useState } from "react";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 const FromMannual = () => {
   const [inputText, setInputText] = useState<string>("");
@@ -18,7 +20,7 @@ const FromMannual = () => {
           Learn how
         </Link>
       </div>
-      <div className="flex flex-col text-lg font-medium">
+      <div className="flex flex-col text-lg font-medium mb-4">
         <p>
           Paste your contact information into this box using commas to separate
           each field.
@@ -26,9 +28,27 @@ const FromMannual = () => {
         <p>There should be one contact per line.</p>
       </div>
       <Textarea
+        className="h-64 mb-8"
         value={inputText}
         onChange={(e) => setInputText(e.target.value)}
       />
+      <div className="flex justify-between">
+        <Button
+          asChild
+          className="w-64 flex gap-x-2 bg-red-700 hover:bg-red-600"
+        >
+          <Link href="/audience/contacts/add">
+            <FaArrowLeft />
+            Back
+          </Link>
+        </Button>
+        <Button asChild className="w-64 flex gap-x-2">
+          <Link href="/">
+            <FaArrowRight />
+            Continue to Organize
+          </Link>
+        </Button>
+      </div>
     </main>
   );
 };
