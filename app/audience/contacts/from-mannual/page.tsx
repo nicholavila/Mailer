1a152b40925dfe913b3d660ce5c1e2ee028426da
nicholavila <nicholavila1@outlook.com>
2024-01-19 14:49:55 +0000

    style: set style to title and description

--- app/audience/contacts/from-mannual/page.tsx
+++ app/audience/contacts/from-mannual/page.tsx
@@ -1,10 +1,19 @@
 "use client";
 
+import Link from "next/link";
+
 const FromMannual = () => {
   return (
     <main className="w-5/6 flex flex-col py-6">
-      <p className="text-4xl font-semibold mb-6">Mannual Input.</p>
-      <p className="text-xl mb-8">Not sure how to format your file?</p>
+      <p className="text-4xl font-semibold mb-6">
+        Copy and paste your contacts
+      </p>
+      <div className="flex items-end gap-x-4">
+        <p className="text-xl">Not sure how to format your file?</p>
+        <Link href="/" className="underline underline-offset-2 text-blue-500">
+          Learn how
+        </Link>
+      </div>
     </main>
   );
 };
