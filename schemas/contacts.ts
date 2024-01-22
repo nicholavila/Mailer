f2b119801018413318324a7ff589b27b9d443ec3
nicholavila <nicholavila1@outlook.com>
2024-01-22 10:33:50 +0000

    rearrange: add schema for new-subscriber

---
+++ schemas/contacts.ts
@@ -0,0 +1,21 @@
+import * as z from "zod";
+
+export const RegisterSchema = z.object({
+  email: z.string().email({ message: "Please enter a valid email" }),
+  firstName: z
+    .string()
+    .min(6, "First name must be at least 6 characters long")
+    .max(32, "First name must be a maximum of 32 characters"),
+  lastName: z
+    .string()
+    .min(6, "Last name must be at least 6 characters long")
+    .max(32, "Last name must be a maximum of 32 characters"),
+  address: z
+    .string()
+    .min(6, "Address must be at least 6 characters long")
+    .max(256, "Address must be a maximum of 256 characters"),
+  phoneNumber: z
+    .string()
+    .min(6, "Last name must be at least 6 characters long")
+    .max(32, "Last name must be a maximum of 32 characters")
+});
