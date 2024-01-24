f9083af9e42dc8f2434e2410d460ef71b7e0500e
nicholavila <nicholavila1@outlook.com>
2024-01-24 14:44:54 +0000

    update: add description in campaign dashboard

--- app/campaign/page.tsx
+++ app/campaign/page.tsx
@@ -1,5 +1,13 @@
 "use client";
 
+const Blue = ({ children }: { children: React.ReactNode }) => {
+  return <span className="text-blue-700">{children}</span>;
+};
+
+const Strong = ({ children }: { children: React.ReactNode }) => {
+  return <span className="font-semibold">{children}</span>;
+};
+
 const CampaignPage = () => {
   return (
     <main className="w-5/6 flex flex-col py-6">
@@ -7,6 +15,17 @@ const CampaignPage = () => {
         Campaign Dashboard
       </p>
       <p className="text-2xl font-semibold pb-2">Hi, Malachi Uy!</p>
+      <p className="text-xl pb-6">
+        Your audience has{" "}
+        <Blue>
+          <Strong>17</Strong>{" "}
+        </Blue>{" "}
+        contacts.{" "}
+        <Blue>
+          <Strong>15</Strong>
+        </Blue>{" "}
+        of these are subscribed.
+      </p>
     </main>
   );
 };
