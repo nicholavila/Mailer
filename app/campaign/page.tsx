744a2c35901e4036804480739a70a4f7a1742958
nicholavila <nicholavila1@outlook.com>
2024-01-24 13:57:10 +0000

    rearrange: add campaign dashboard

---
+++ app/campaign/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+const CampaignPage = () => {
+  return (
+    <main className="w-5/6 flex flex-col py-6">
+      <p className="text-5xl text-green-700 font-semibold pb-6">
+        Campaign Dashboard
+      </p>
+      <p className="text-2xl font-semibold pb-2">Hi, Malachi Uy!</p>
+    </main>
+  );
+};
+
+export default CampaignPage;
