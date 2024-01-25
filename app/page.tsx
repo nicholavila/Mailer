396854f87d99011ee6c1759f631f0245f12d4667
nicholavila <nicholavila1@outlook.com>
2024-01-25 15:24:38 +0000

    update layout for main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,11 @@
 "use client";
 
 import React from "react";
-import { Poppins } from "next/font/google";
-import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
 import { useCurrentUser } from "@/hooks/use-current-user";
-import Link from "next/link";
 import { SignupButton } from "@/components/auth/signup-button";
 import { WrappedButton } from "@/components/utils/wrapped-button";
 
-const font = Poppins({
-  subsets: ["latin"],
-  weight: ["600"]
-});
-
 const Home = () => {
-  const user = useCurrentUser();
-
   return (
     <main className="w-full flex flex-col items-center pt-24 pb-6">
       <p className="text-5xl font-semibold pb-6">Turn Emails into Revenue!</p>
