b09e758e29d38f5f7fe7144c261904418a0e15af
nicholavila <nicholavila1@outlook.com>
2024-01-25 12:30:41 +0000

    update: update dynamodb adapter

--- adapter.ts
+++ adapter.ts
@@ -12,30 +12,26 @@ export default {
   async getUserByAccount(
     providerAccountId: Pick<AdapterAccount, "provider" | "providerAccountId">
   ): Awaitable<AdapterUser | null> {
-    console.log("__getUserByAccount", providerAccountId);
     // { providerAccountId: '126603430', provider: 'github' }
     return null;
   },
   async getUser(id: string): Awaitable<AdapterUser | null> {
-    console.log("__getUser", id);
     // 8876a5dc-7248-4682-a255-5412417373dd
     return null;
   },
   async getUserByEmail(email: string): Awaitable<AdapterUser | null> {
-    console.log("__getUserByEmail", email);
-    // sacreddevking@gmail.com
+    // andrei.devcasian@gmail.com
     return null;
   },
   async createUser(user: AdapterUser): Awaitable<AdapterUser> {
-    console.log("__createUser", user);
     const existingUser = await getUserByEmail(user.email);
     if (!existingUser) {
       await createUser({ ...user, emailVerified: new Date() });
     }
     // {
     //   id: '2e79e340-fb52-4e65-a535-a656734b5b3f',
-    //   name: 'DevKing',
-    //   email: 'sacreddevking@gmail.com',
+    //   name: 'Andrei',
+    //   email: 'andrei.devcasian@gmail.com',
     //   image: 'https://avatars.githubusercontent.com/u/126603430?v=4',
     //   emailVerified: null
     // }
@@ -44,7 +40,6 @@ export default {
   async linkAccount(
     account: AdapterAccount
   ): Promise<void> | Awaitable<AdapterAccount | null | undefined> {
-    console.log("__linkAccount", account);
     // {
     //   access_token: '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',
     //   scope: 'read:user,user:email',
