d92a7a3c9e38d29e4adec09c76c64e063b4dca97
nicholavila <nicholavila1@outlook.com>
2024-01-25 09:40:26 +0000

    style: upgrade style of email box

--- app/campaign/_components/campaign-item.tsx
+++ app/campaign/_components/campaign-item.tsx
@@ -1,10 +1,32 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { FaEdit, FaMailBulk, FaMailchimp, FaVoicemail } from "react-icons/fa";
+
 const CampaignItem = () => {
   return (
-    <div className="w-full">
+    <div className="w-full flex justify-between">
+      <div className="flex gap-x-8">
+        <FaMailBulk className="text-4xl text-yellow-700" />
+        <div className="flex flex-col gap-y-2 text-gray-500">
+          <p className="text-xl text-blue-500">Email Title</p>
+          <p className="text-base">Regular · Malachi Uy</p>
+          <p className="text-base">Tags: Customer</p>
+          <p className="text-base">Edited Wed, April 10th 11:25 pm by you</p>
+        </div>
+      </div>
+      <div className="flex flex-col gap-y-2">
+        <Badge className="w-fit text-base font-normal text-black text-center bg-gray-200">
+          Draft
+        </Badge>
+        <Badge className="text-base font-normal text-black text-center bg-gray-200">
+          New Builder
+        </Badge>
+      </div>
       <div className="flex flex-col">
-        <p className="text-xl">Email Title</p>
-        <p className="text-xl">Tags: Customer</p>
-        <p className="text-xl">Edited Wed, April 10th 11:25 pm by you</p>
+        <Button className="flex gap-x-2 w-32 text-black bg-green-500">
+          <FaEdit />
+          Edit
+        </Button>
       </div>
     </div>
   );
