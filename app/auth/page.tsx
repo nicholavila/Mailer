ff14e8a826524d5d0e22017d3e57940622fec0d1
nicholavila <nicholavila1@outlook.com>
2024-01-25 14:52:44 +0000

    update: update general view of auth layout

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,7 +1,5 @@
 import { Poppins } from "next/font/google";
 import { cn } from "@/lib/utils";
-import { LoginButton } from "@/components/auth/login-button";
-import { WrappedButton } from "@/components/utils/wrapped-button";
 
 const font = Poppins({
   subsets: ["latin"],
@@ -11,17 +9,15 @@ const font = Poppins({
 export default function Home() {
   return (
     <main className="space-y-6 text-center">
-      <h1 className={cn("text-6xl font-semibold text-black drop-shadow-md", font.className)}>
+      <h1
+        className={cn(
+          "text-6xl font-semibold text-black drop-shadow-md",
+          font.className
+        )}
+      >
         🔐 Auth
       </h1>
       <p className="text-black text-lg">Authentication service for Kre8tive</p>
-      {/* <div>
-          <LoginButton>
-            <WrappedButton variant="default" size="lg">
-              Sign in
-            </WrappedButton>
-          </LoginButton>
-        </div> */}
     </main>
   );
 }
