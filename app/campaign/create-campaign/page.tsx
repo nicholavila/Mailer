404806481044f41973beaa736296840d76829959
nicholavila <nicholavila1@outlook.com>
2024-01-25 10:53:06 +0000

    update: update create campaign page

--- app/campaign/create-campaign/page.tsx
+++ app/campaign/create-campaign/page.tsx
@@ -4,33 +4,19 @@ import { Button } from "@/components/ui/button";
 import { FaPlus } from "react-icons/fa";
 import { Separator } from "@/components/ui/separator";
 import Link from "next/link";
-
-const Blue = ({ children }: { children: React.ReactNode }) => {
-  return <span className="text-blue-700">{children}</span>;
-};
-
-const Strong = ({ children }: { children: React.ReactNode }) => {
-  return <span className="font-semibold">{children}</span>;
-};
+import { Input } from "@/components/ui/input";
 
 const NewCampaign = () => {
   return (
     <main className="w-5/6 flex flex-col py-6">
-      <p className="text-5xl text-green-700 font-semibold pb-6">
+      <p className="text-5xl text-green-700 font-semibold pb-4">
         Create a new Campaign
       </p>
-      <p className="text-2xl font-semibold pb-2">Hi, Malachi Uy!</p>
-      <p className="text-xl">
-        Your audience has{" "}
-        <Blue>
-          <Strong>17</Strong>{" "}
-        </Blue>{" "}
-        contacts.{" "}
-        <Blue>
-          <Strong>15</Strong>
-        </Blue>{" "}
-        of these are subscribed.
+      <p className="text-xl text-gray-500 pb-12">
+        Reach your customer's inboxes at the right time with Campaign Manager.
       </p>
+      <p className="text-2xl font-semibold pb-2">Internet email name</p>
+      <Input type="text" className="w-96 h-12 border border-green-700" />
     </main>
   );
 };
