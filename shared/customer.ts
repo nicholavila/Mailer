9ad9fcdd95fe5111d65701b81a9a9a5f075811f8
nicholavila <nicholavila1@outlook.com>
2024-01-29 14:23:41 +0000

    feat: add cutomer global type

---
+++ shared/customer.ts
@@ -0,0 +1,13 @@
+export type Customer = {
+  email: string;
+  firstName: string;
+  lastName: string;
+  address: string;
+  phone: string;
+  birthday: string;
+  tags: string[];
+  subscribed: boolean;
+  contactRating: number;
+  created: string;
+  lastChanged: string;
+};
