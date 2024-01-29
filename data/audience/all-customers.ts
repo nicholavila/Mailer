1420050e3e21b2d485976f475db8acb174c2d8b0
nicholavila <nicholavila1@outlook.com>
2024-01-29 13:50:23 +0000

    rearrange: add all-cutomer file

---
+++ data/audience/all-customers.ts
@@ -0,0 +1,71 @@
+"use server";
+
+export const getAllCustomersByEmail = async (email: string) => {
+  return [
+    {
+      email: "ken99@yahoo.com",
+      firstName: "success",
+      lastName: "Last",
+      address: "address1 address2 address3",
+      phone: "1215646785",
+      birthday: "07/05/1994",
+      tags: ["Customer1", "Customer2"],
+      subscribed: true,
+      contactRating: 2,
+      created: "06/05/2024",
+      lastChanged: "06/05/2024"
+    },
+    {
+      email: "ken99@yahoo.com",
+      firstName: "success",
+      lastName: "Last",
+      address: "address1 address2 address3",
+      phone: "1215646785",
+      birthday: "07/05/1994",
+      tags: ["Customer1", "Customer3"],
+      subscribed: true,
+      contactRating: 2,
+      created: "06/05/2024",
+      lastChanged: "06/05/2024"
+    },
+    {
+      email: "ken99@yahoo.com",
+      firstName: "success",
+      lastName: "Last",
+      address: "address1 address2 address3",
+      phone: "1215646785",
+      birthday: "07/05/1994",
+      tags: ["Customer2"],
+      subscribed: false,
+      contactRating: 2,
+      created: "06/05/2024",
+      lastChanged: "06/05/2024"
+    },
+    {
+      email: "ken99@yahoo.com",
+      firstName: "success",
+      lastName: "Last",
+      address: "address1 address2 address3",
+      phone: "1215646785",
+      birthday: "07/05/1994",
+      tags: ["Customer3"],
+      subscribed: true,
+      contactRating: 2,
+      created: "06/05/2024",
+      lastChanged: "06/05/2024"
+    },
+    {
+      email: "Abe45@gmail.com",
+      firstName: "success",
+      lastName: "Last",
+      address: "address1 address2 address3",
+      phone: "1215646785",
+      birthday: "07/05/1994",
+      tags: ["Customer1"],
+      subscribed: true,
+      contactRating: 2,
+      created: "06/05/2024",
+      lastChanged: "06/05/2024"
+    }
+  ];
+};
