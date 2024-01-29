5afa90343f48a879080586205f8af8e00bc908c7
nicholavila <nicholavila1@outlook.com>
2024-01-29 12:00:44 +0000

    chore: convert static data into dynamic

--- app/audience/contacts/page.tsx
+++ app/audience/contacts/page.tsx
@@ -59,73 +59,77 @@ export default function Contacts() {
   const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
   const [rowSelection, setRowSelection] = useState({});
 
-  const customers: Customer[] = [
-    {
-      email: "ken99@yahoo.com",
-      firstName: "success",
-      lastName: "Last",
-      address: "address1 address2 address3",
-      phone: "1215646785",
-      birthday: "07/05/1994",
-      tags: ["Customer1", "Customer2"],
-      subscribed: true,
-      contactRating: 2,
-      created: "06/05/2024",
-      lastChanged: "06/05/2024"
-    },
-    {
-      email: "ken99@yahoo.com",
-      firstName: "success",
-      lastName: "Last",
-      address: "address1 address2 address3",
-      phone: "1215646785",
-      birthday: "07/05/1994",
-      tags: ["Customer1", "Customer3"],
-      subscribed: true,
-      contactRating: 2,
-      created: "06/05/2024",
-      lastChanged: "06/05/2024"
-    },
-    {
-      email: "ken99@yahoo.com",
-      firstName: "success",
-      lastName: "Last",
-      address: "address1 address2 address3",
-      phone: "1215646785",
-      birthday: "07/05/1994",
-      tags: ["Customer2"],
-      subscribed: false,
-      contactRating: 2,
-      created: "06/05/2024",
-      lastChanged: "06/05/2024"
-    },
-    {
-      email: "ken99@yahoo.com",
-      firstName: "success",
-      lastName: "Last",
-      address: "address1 address2 address3",
-      phone: "1215646785",
-      birthday: "07/05/1994",
-      tags: ["Customer3"],
-      subscribed: true,
-      contactRating: 2,
-      created: "06/05/2024",
-      lastChanged: "06/05/2024"
-    },
-    {
-      email: "Abe45@gmail.com",
-      firstName: "success",
-      lastName: "Last",
-      address: "address1 address2 address3",
-      phone: "1215646785",
-      birthday: "07/05/1994",
-      tags: ["Customer1"],
-      subscribed: true,
-      contactRating: 2,
-      created: "06/05/2024",
-      lastChanged: "06/05/2024"
-    }
-  ];
+  const [customers, setCustomers] = useState<Customer[]>([]);
+
+  useEffect(() => {
+    setCustomers([
+      {
+        email: "ken99@yahoo.com",
+        firstName: "success",
+        lastName: "Last",
+        address: "address1 address2 address3",
+        phone: "1215646785",
+        birthday: "07/05/1994",
+        tags: ["Customer1", "Customer2"],
+        subscribed: true,
+        contactRating: 2,
+        created: "06/05/2024",
+        lastChanged: "06/05/2024"
+      },
+      {
+        email: "ken99@yahoo.com",
+        firstName: "success",
+        lastName: "Last",
+        address: "address1 address2 address3",
+        phone: "1215646785",
+        birthday: "07/05/1994",
+        tags: ["Customer1", "Customer3"],
+        subscribed: true,
+        contactRating: 2,
+        created: "06/05/2024",
+        lastChanged: "06/05/2024"
+      },
+      {
+        email: "ken99@yahoo.com",
+        firstName: "success",
+        lastName: "Last",
+        address: "address1 address2 address3",
+        phone: "1215646785",
+        birthday: "07/05/1994",
+        tags: ["Customer2"],
+        subscribed: false,
+        contactRating: 2,
+        created: "06/05/2024",
+        lastChanged: "06/05/2024"
+      },
+      {
+        email: "ken99@yahoo.com",
+        firstName: "success",
+        lastName: "Last",
+        address: "address1 address2 address3",
+        phone: "1215646785",
+        birthday: "07/05/1994",
+        tags: ["Customer3"],
+        subscribed: true,
+        contactRating: 2,
+        created: "06/05/2024",
+        lastChanged: "06/05/2024"
+      },
+      {
+        email: "Abe45@gmail.com",
+        firstName: "success",
+        lastName: "Last",
+        address: "address1 address2 address3",
+        phone: "1215646785",
+        birthday: "07/05/1994",
+        tags: ["Customer1"],
+        subscribed: true,
+        contactRating: 2,
+        created: "06/05/2024",
+        lastChanged: "06/05/2024"
+      }
+    ]);
+  }, []);
 
   const columns: ColumnDef<Customer>[] = [
     {
