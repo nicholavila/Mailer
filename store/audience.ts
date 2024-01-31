d16a0af3c985324312d62f5e02e1c224634ccff6
nicholavila <nicholavila1@outlook.com>
2024-01-31 12:53:08 +0000

    configure atom

--- store/audience.ts
+++ store/audience.ts
@@ -2,6 +2,15 @@ import { atom } from "jotai";
 
 const customersAtom = atom([]);
 
-const readOnlyAtom = atom((get) => get(customersAtom));
+const readCutomerAtom = atom((get) => get(customersAtom));
+const writeCustomerAtom = atom(null, (get, set, updateValue) => {
+  set(customersAtom, (prev) => [...prev, updateValue]);
+});
+const readWriteCustomerAtom = atom(
+  (get) => get(customersAtom),
+  (get, set, updateValue) => {
+    set(customersAtom, (prev) => [...prev, updateValue]);
+  }
+);
 
 export { customersAtom };
