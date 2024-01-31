005ad74761f96390345be79435e02789556b5f5a
nicholavila <nicholavila1@outlook.com>
2024-01-31 13:14:23 +0000

    refactor: fix typescript issue in audience

--- store/audience.ts
+++ store/audience.ts
@@ -1,15 +1,16 @@
+import { Customer } from "@/shared/customer-type";
 import { atom } from "jotai";
 
-const customersAtom = atom([]);
+const customersAtom = atom<Customer[]>([]);
 
 const readCutomerAtom = atom((get) => get(customersAtom));
-const writeCustomerAtom = atom(null, (get, set, updateValue) => {
-  set(customersAtom, (prev) => [...prev, updateValue]);
+const writeCustomerAtom = atom(null, (get, set, updateData: Customer) => {
+  set(customersAtom, (prev) => [...prev, updateData]);
 });
 const readWriteCustomerAtom = atom(
   (get) => get(customersAtom),
-  (get, set, updateValue) => {
-    set(customersAtom, (prev) => [...prev, updateValue]);
+  (get, set, updateData: Customer) => {
+    set(customersAtom, (prev) => [...prev, updateData]);
   }
 );
 
