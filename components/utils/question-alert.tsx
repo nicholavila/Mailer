b52c4d9b875895bed1fe318c1523e100254b98fa
nicholavila <nicholavila1@outlook.com>
2024-02-01 11:19:52 +0000

    rearrange: add question-alert

---
+++ components/utils/question-alert.tsx
@@ -0,0 +1,37 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle
+} from "@/components/ui/alert-dialog";
+
+type PropsParams = {
+  open: boolean;
+  title: string;
+  description: string;
+  onAlertDialogClosed: (open: boolean) => void;
+};
+
+export const QuestionAlert = ({
+  open,
+  title,
+  description,
+  onAlertDialogClosed
+}: PropsParams) => {
+  return (
+    <AlertDialog open={open} onOpenChange={onAlertDialogClosed}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{title}</AlertDialogTitle>
+          <AlertDialogDescription>{description}</AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogAction className="w-24">OK</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
