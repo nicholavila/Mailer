ac107eaa1208b42b9e1bd92df6ed0fed38674366
nicholavila <nicholavila1@outlook.com>
2024-02-02 12:55:23 +0000

    rearrange: create-subscriber data func

--- data/audience/create-subscriber.ts
+++ data/audience/create-subscriber.ts
@@ -1,23 +1,9 @@
 import db from "@/lib/db";
 import { GetCommand, PutCommand, UpdateCommand } from "@aws-sdk/lib-dynamodb";
 import { generateVerificationToken } from "@/lib/tokens";
+import { NewSubscriber } from "@/shared/customer-type";
 
 const TableName = process.env.AWS_DYNAMODB_MAILING_LIST_TABLE_NAME;
-
-interface NewSubscriber {
-  ownerEmail: string;
-  customerEmail: string;
-  firstName: string;
-  lastName: string;
-  address: string;
-  phone: string;
-  birthday: string;
-  tags: string[];
-  subscribed: boolean;
-  contactRating: number;
-  created: string;
-  lastChanged: string;
-}
 
 export const createSubscriber = async (data: NewSubscriber) => {
   const command = new PutCommand({
@@ -30,6 +16,7 @@ export const createSubscriber = async (data: NewSubscriber) => {
   try {
     const response = await db.send(command);
     console.log("__createSubscriber__PutCommand__RESPONSE", response);
+    return response;
   } catch (error) {
     console.log("__createSubscriber__PutCommand__ERROR", error);
     return null;
