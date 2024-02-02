f3e70953e31e9ceb8496ecd07f93899cf6ab4dee
nicholavila <nicholavila1@outlook.com>
2024-02-02 11:56:38 +0000

    rearrange: rename new-subscriber action

--- actions/audience/new-subscriber.ts
+++ actions/audience/new-subscriber.ts
@@ -2,14 +2,16 @@
 
 import * as z from "zod";
 import { NewSubscriberSchema } from "@/schemas/contacts";
+import { createSubscriber } from "@/data/audience/create-subscriber";
+import { NewSubscriber } from "@/shared/customer-type";
 
-export const createNewSubscriber = async (
-  values: z.infer<typeof NewSubscriberSchema>
-) => {
+export const newSubscriber = async (values: NewSubscriber) => {
   const validatedFields = NewSubscriberSchema.safeParse(values);
   if (!validatedFields.success) {
     return { error: "Invalid fields!" };
   }
+
+  await createSubscriber({});
 
   return { success: "Password updated!" };
 };
