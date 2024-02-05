7fd5d0bb8fbaac2432b4a740686b350a224ca918
nicholavila <nicholavila1@outlook.com>
2024-02-05 12:38:00 +0000

    feat: add new-subscriber func

--- actions/audience/new-subscriber.ts
+++ actions/audience/new-subscriber.ts
@@ -1,10 +1,41 @@
 "use server";
 
 import { createSubscriber } from "@/data/audience/create-subscriber";
-import { NewSubscriber } from "@/shared/customer-type";
+import { getSubscriberByEmail } from "@/data/audience/subscriber-by-email";
+import { updateCustomer } from "@/data/audience/update-cusomer";
+import { Customer } from "@/shared/customer-type";
 
-export const newSubscriber = async (values: NewSubscriber) => {
-  const response = await createSubscriber(values);
+export const newSubscriber = async (
+  values: Customer,
+  updateChecked: boolean
+) => {
+  const existingSubscriber = await getSubscriberByEmail({
+    ownerEmail: values.ownerEmail,
+    customerEmail: values.customerEmail
+  });
+
+  if (existingSubscriber && !updateChecked) {
+    return { error: "Subscriber with same email already exists!" };
+  }
+
+  if (existingSubscriber) {
+    const response = await updateCustomer({
+      ...values,
+      lastChanged: new Date().toISOString()
+    });
+
+    if (response.success) {
+      return { success: "Subscriber was updated successfully!" };
+    } else {
+      return { error: response.error };
+    }
+  }
+
+  const created = new Date().toISOString();
+  const lastChanged = created;
+
+  const response = await createSubscriber({ ...values, created, lastChanged });
+
   if (response.success) {
     return { success: "New subscriber registered!" };
   } else {
