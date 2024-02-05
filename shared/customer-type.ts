9bb78c15c911cb4b833cdfd4347054766f4c3a78
nicholavila <nicholavila1@outlook.com>
2024-02-05 09:43:36 +0000

    update customer interface

--- shared/customer-type.ts
+++ shared/customer-type.ts
@@ -1,18 +1,4 @@
 export type Customer = {
-  email: string;
-  firstName: string;
-  lastName: string;
-  address: string;
-  phone: string;
-  birthday: string;
-  tags: string[];
-  subscribed: boolean;
-  contactRating: number;
-  created: string;
-  lastChanged: string;
-};
-
-export interface NewSubscriber {
   ownerEmail: string;
   customerEmail: string;
   firstName: string;
@@ -25,4 +11,4 @@ export interface NewSubscriber {
   contactRating?: number;
   created?: string;
   lastChanged?: string;
-}
+};
