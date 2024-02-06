0ec1c2e706ce7450f806ee0b42167a8e4ba6b814
nicholavila <nicholavila1@outlook.com>
2024-02-06 09:11:53 +0000

    feat: update customer

---
+++ data/audience/update-cusomer.ts
@@ -0,0 +1,25 @@
+import db from "@/lib/db";
+import { UpdateCommand } from "@aws-sdk/lib-dynamodb";
+import { Customer } from "@/shared/customer-type";
+
+const TableName = process.env.AWS_DYNAMODB_MAILING_LIST_TABLE_NAME;
+
+export const updateCustomer = async (data: Customer) => {
+  const command = new UpdateCommand({
+    TableName,
+    Key: { ownerEmail: data.ownerEmail, customerEmail: data.customerEmail },
+    UpdateExpression:
+      "SET firstName = :firstName, lastName = :lastName, address = :address, phoneNumber = :phoneNumber, birthday = :birthday, tags = :tags, subscribed = :subscribed, lastChanged = :lastChanged",
+
+    ReturnValues: "ALL_NEW"
+  });
+
+  try {
+    const response = await db.send(command);
+    console.log("__updateUserToken__UpdateCommand__RESPONSE", response);
+    return { success: true };
+  } catch (error) {
+    console.log("__updateUserToken__UpdateCommand__ERROR", error);
+    return { error };
+  }
+};
