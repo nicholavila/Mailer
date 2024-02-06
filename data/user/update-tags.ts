2bd245cf95b102982ee2f0cfe0b3a156879fce48
nicholavila <nicholavila1@outlook.com>
2024-02-06 13:21:30 +0000

    rearrange: update user tags

---
+++ data/user/update-tags.ts
@@ -0,0 +1,34 @@
+import db from "@/lib/db";
+import { GetCommand, PutCommand, UpdateCommand } from "@aws-sdk/lib-dynamodb";
+import { generateVerificationToken } from "@/lib/tokens";
+
+const TableName = process.env.AWS_DYNAMODB_USER_TABLE_NAME;
+
+interface UserSetToken {
+  email: string;
+  verificationToken: string;
+  expires: Date;
+}
+
+export const updateUserTags = async (data: UserSetToken) => {
+  const command = new UpdateCommand({
+    TableName,
+    Key: { email: data.email },
+    UpdateExpression:
+      "SET verificationToken = :verificationToken, expires = :expires",
+    ExpressionAttributeValues: {
+      ":verificationToken": data.verificationToken,
+      ":expires": data.expires.toISOString()
+    },
+    ReturnValues: "ALL_NEW"
+  });
+
+  try {
+    const response = await db.send(command);
+    console.log("__updateUserTags__UpdateCommand__RESPONSE", response);
+    return response.Attributes;
+  } catch (error) {
+    console.log("__updateUserTags__UpdateCommand__ERROR", error);
+    return null;
+  }
+};
