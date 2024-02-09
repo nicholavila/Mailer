c40a56efff96349b9bc66bdf74c7d8e8ca5abe33
nicholavila <nicholavila1@outlook.com>
2024-02-09 09:42:22 +0000

    update style for segment page

--- app/audience/segments/page.tsx
+++ app/audience/segments/page.tsx
@@ -4,11 +4,11 @@ import { FaPlus } from "react-icons/fa";
 
 const Segments = () => {
   return (
-    <main className="w-full">
+    <main className="w-5/6 flex flex-col py-6">
       <div className="w-full flex items-end justify-between pb-6">
-        <p className="text-4xl text-green-700 font-semibold">All Contacts</p>
+        <p className="text-4xl text-green-700 font-semibold">All Segments</p>
         <Button variant="default" asChild className="w-64 flex gap-x-4">
-          <Link href="/audience/contacts/add">
+          <Link href="/audience/segments/add">
             <FaPlus />
             Add Segment
           </Link>
