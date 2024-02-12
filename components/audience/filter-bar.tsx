6476002e6a6a6fa94d9dd1f59cf01fe0044031b1
nicholavila <nicholavila1@outlook.com>
2024-02-12 13:26:45 +0000

    add select to filterbar

--- components/audience/filter-bar.tsx
+++ components/audience/filter-bar.tsx
@@ -10,7 +10,22 @@ import {
 
 export const FilterBar = () => {
   return (
-    <main className="w-full px-6 py-4 border rounded-lg">
+    <main className="w-full flex items-center gap-x-6 px-6 py-4 border rounded-lg">
+      <Select>
+        <SelectTrigger className="w-[240px]">
+          <SelectValue placeholder="Select or search a filter" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel>Fruits</SelectLabel>
+            <SelectItem value="apple">Apple</SelectItem>
+            <SelectItem value="banana">Banana</SelectItem>
+            <SelectItem value="blueberry">Blueberry</SelectItem>
+            <SelectItem value="grapes">Grapes</SelectItem>
+            <SelectItem value="pineapple">Pineapple</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
       <Select>
         <SelectTrigger className="w-[240px]">
           <SelectValue placeholder="Select or search a filter" />
