46adc33f5eff4a2d6c4925cc9069f6afb868a523
nicholavila <nicholavila1@outlook.com>
2024-02-12 11:29:07 +0000

    add description for add-segment

--- components/audience/add-segment.tsx
+++ components/audience/add-segment.tsx
@@ -1,6 +1,15 @@
+import { FaFilter } from "react-icons/fa";
+
 const AddSement = () => {
-  return;
-  <main className="w-full">Regular Segment Creation</main>;
+  return (
+    <main className="w-full">
+      <p className="text-xl font-semibold">Regular Segment Builder</p>
+      <div className="flex items-center gap-2">
+        <FaFilter />
+        Segment Filters
+      </div>
+    </main>
+  );
 };
 
 export default AddSement;
