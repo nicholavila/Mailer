f9d90b50d8a2ef30cc4712ef83891972266c074c
nicholavila <nicholavila1@outlook.com>
2024-02-13 11:57:17 +0000

    add filter input for tagscondition

--- components/audience/filter-bar.tsx
+++ components/audience/filter-bar.tsx
@@ -14,21 +14,25 @@ import {
   isTypeOfTags
 } from "@/lib/filter-customer";
 import { useState } from "react";
+import { Input } from "../ui/input";
 
 const TagsCondition = () => {
   return (
-    <Select>
-      <SelectTrigger className="w-[240px] px-4 bg-gray-200 rounded-full">
-        <SelectValue placeholder="Select a condition" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          {/* <SelectLabel>Select a Condition</SelectLabel> */}
-          <SelectItem value="contains">Contains</SelectItem>
-          <SelectItem value="not-contains">Doesn't contain</SelectItem>
-        </SelectGroup>
-      </SelectContent>
-    </Select>
+    <div className="flex gap-x-6">
+      <Select>
+        <SelectTrigger className="w-[240px] px-4 bg-gray-200 rounded-full">
+          <SelectValue placeholder="Select a condition" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            {/* <SelectLabel>Select a Condition</SelectLabel> */}
+            <SelectItem value="contains">Contains</SelectItem>
+            <SelectItem value="not-contains">Doesn't contain</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+      <Input />
+    </div>
   );
 };
 
