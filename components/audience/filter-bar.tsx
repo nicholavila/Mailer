ee92a5f4a5d6d12b28b2d9355787621f17debb90
nicholavila <nicholavila1@outlook.com>
2024-02-13 08:31:12 +0000

    feat: add string condition component

--- components/audience/filter-bar.tsx
+++ components/audience/filter-bar.tsx
@@ -8,17 +8,43 @@ import {
   SelectValue
 } from "@/components/ui/select";
 import { filterAttributes } from "@/lib/filter-customer";
+import { useState } from "react";
+
+const StringCondition = () => {
+  return (
+    <Select>
+      <SelectTrigger className="w-[240px] px-4 bg-gray-200 rounded-full">
+        <SelectValue placeholder="Select a condition" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          {/* <SelectLabel>Select a Condition</SelectLabel> */}
+          <SelectItem value="contains">Containts</SelectItem>
+          <SelectItem value="not-contains">Containts</SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+};
 
 export const FilterBar = () => {
+  const [attribute, setAttribute] = useState<string>();
+  const [condition, setCondition] = useState<string>();
+  const [filter, setFilter] = useState<string>();
+
+  const onAttributeChange = (value: string) => {
+    setAttribute(value);
+  };
+
   return (
     <main className="w-full flex items-center gap-x-6 px-6 py-4 border rounded-none">
-      <Select>
+      <Select onValueChange={onAttributeChange}>
         <SelectTrigger className="w-[240px] px-4 bg-gray-200 rounded-full">
           <SelectValue placeholder="Select or search a filter" />
         </SelectTrigger>
         <SelectContent>
           <SelectGroup>
-            <SelectLabel>Select a Key</SelectLabel>
+            {/* <SelectLabel>Select a Key</SelectLabel> */}
             {filterAttributes.map((attribute, index) => (
               <SelectItem key={attribute.value} value={attribute.value}>
                 {attribute.name}
@@ -27,6 +53,7 @@ export const FilterBar = () => {
           </SelectGroup>
         </SelectContent>
       </Select>
+      <StringCondition />
     </main>
   );
 };
