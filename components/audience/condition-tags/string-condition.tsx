3139e455f502aebdb8b7c4b2c9726cf6bbb6b283
nicholavila <nicholavila1@outlook.com>
2024-02-16 12:14:45 +0000

    rearrange: string condition

--- components/audience/condition-tags/string-condition.tsx
+++ components/audience/condition-tags/string-condition.tsx
@@ -1,3 +1,4 @@
+import { Input } from "@/components/ui/input";
 import {
   Select,
   SelectContent,
@@ -12,21 +13,34 @@ import { FilterType } from "@/shared/filter-type";
 type PropsType = {
   filter: FilterType;
   onConditionChange: (value: string) => void;
+  onValueChange: (value: string) => void;
 };
 
-export const StringCondition = ({ filter, onConditionChange }: PropsType) => {
+export const StringCondition = ({
+  filter,
+  onConditionChange,
+  onValueChange
+}: PropsType) => {
   return (
-    <Select onValueChange={onConditionChange}>
-      <SelectTrigger className="w-[240px] px-4 bg-gray-200 rounded-full">
-        <SelectValue placeholder="Select a condition" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          {/* <SelectLabel>Select a Condition</SelectLabel> */}
-          <SelectItem value="contains">Contains</SelectItem>
-          <SelectItem value="not-contains">Doesn't contain</SelectItem>
-        </SelectGroup>
-      </SelectContent>
-    </Select>
+    <div className="flex items-center gap-x-6">
+      <Select value={filter.condition} onValueChange={onConditionChange}>
+        <SelectTrigger className="w-[240px] px-4 bg-gray-200 rounded-full">
+          <SelectValue placeholder="Select a condition" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            {/* <SelectLabel>Select a Condition</SelectLabel> */}
+            <SelectItem value="contains">Contains</SelectItem>
+            <SelectItem value="not-contains">Doesn't contain</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+      <Input
+        placeholder="Enter a value"
+        className="w-[240px] px-4 rounded-full"
+        value={filter.value}
+        onChange={(event) => onValueChange(event.target.value)}
+      />
+    </div>
   );
 };
