b0678453bb32ed78bd755d0c94ee8de5f177d2c4
nicholavila <nicholavila1@outlook.com>
2024-02-16 09:42:49 +0000

    complete tag condition component

--- components/audience/condition-tags/tag-condition.tsx
+++ components/audience/condition-tags/tag-condition.tsx
@@ -11,22 +11,44 @@ import { FilterType } from "@/shared/filter-type";
 
 type PropsType = {
   filter: FilterType;
+  storedTags: string[];
   onConditionChange: (value: string) => void;
+  onValueChange: (value: string) => void;
 };
 
-export const TagsCondition = ({ filter, onConditionChange }: PropsType) => {
+export const TagsCondition = ({
+  filter,
+  storedTags,
+  onConditionChange,
+  onValueChange
+}: PropsType) => {
   return (
-    <Select onValueChange={onConditionChange}>
-      <SelectTrigger className="w-[240px] px-4 bg-gray-200 rounded-full">
-        <SelectValue placeholder="Select a condition" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          {/* <SelectLabel>Select a Condition</SelectLabel> */}
-          <SelectItem value="contains">Contains</SelectItem>
-          <SelectItem value="not-contains">Doesn't contain</SelectItem>
-        </SelectGroup>
-      </SelectContent>
-    </Select>
+    <div className="flex items-center gap-x-6">
+      <Select value={filter.condition} onValueChange={onConditionChange}>
+        <SelectTrigger className="w-[240px] px-4 bg-gray-200 rounded-full">
+          <SelectValue placeholder="Select a condition" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            {/* <SelectLabel>Select a Condition</SelectLabel> */}
+            <SelectItem value="contains">Contains</SelectItem>
+            <SelectItem value="not-contains">Doesn't contain</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+      <Select value={filter.value} onValueChange={onValueChange}>
+        <SelectTrigger className="w-[240px] px-4 bg-gray-200 rounded-full">
+          <SelectValue placeholder="Select a tag" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            {/* <SelectLabel>Select a Condition</SelectLabel> */}
+            {storedTags.map((tag) => (
+              <SelectItem value={tag}>{tag}</SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </div>
   );
 };
