4512a20264cce104ca7d2c1302528c1b2c4a89f8
nicholavila <nicholavila1@outlook.com>
2024-02-19 12:17:01 +0000

    rearrange: delete Customer action

---
+++ data/audience/delete-customer.ts
@@ -0,0 +1,23 @@
+import db from "@/lib/db";
+import { DeleteCommand } from "@aws-sdk/lib-dynamodb";
+
+const TableName = process.env.AWS_DYNAMODB_MAILING_LIST_TABLE_NAME;
+
+export const deleteCutomer = async (
+  ownerEmail: string,
+  customerEmail: string
+) => {
+  const command = new DeleteCommand({
+    TableName,
+    Key: { ownerEmail, customerEmail }
+  });
+
+  try {
+    const response = await db.send(command);
+    console.log("__deleteCutomer__DeleteCommand__RESPONSE", response);
+    return { success: true, response };
+  } catch (error) {
+    console.log("__deleteCutomer__DeleteCommand__ERROR", error);
+    return { error };
+  }
+};
