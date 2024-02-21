e12ead2f062a3d30151a8375c34d19f1e30c8f8b
nicholavila <nicholavila1@outlook.com>
2024-02-21 14:55:28 +0000

    update button for open menu

--- app/audience/_components/segments-column.tsx
+++ app/audience/_components/segments-column.tsx
@@ -103,7 +103,11 @@ export const getColumnsForSegmentsTable = ({
         return (
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
-              <Button variant="ghost" className="h-8 w-8 p-0">
+              <Button
+                disabled={isPending}
+                variant="ghost"
+                className="h-8 w-8 p-0"
+              >
                 <span className="sr-only">Open menu</span>
                 <MoreHorizontal className="h-4 w-4" />
               </Button>
