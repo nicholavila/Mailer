3d1bcbf8dadd8bc07e9d4ad619b7d74409f02feb
nicholavila <nicholavila1@outlook.com>
2024-02-22 16:02:47 +0000

    feat: add action for delete segment

---
+++ data/segment/delete-segment.ts
@@ -0,0 +1,22 @@
+"use server";
+
+import db from "@/lib/db";
+import { DeleteCommand } from "@aws-sdk/lib-dynamodb";
+
+const TableName = process.env.AWS_DYNAMODB_SEGMENTS_TABLE_NAME;
+
+export const deleteSegment = async (userEmail: string, segmentId: string) => {
+  const command = new DeleteCommand({
+    TableName,
+    Key: { userEmail, segmentId }
+  });
+
+  try {
+    const response = await db.send(command);
+    console.log("__deleteSegment__DeleteCommand__RESPONSE", response);
+    return { success: true, response };
+  } catch (error) {
+    console.log("__deleteSegment__DeleteCommand__ERROR", error);
+    return { error };
+  }
+};
