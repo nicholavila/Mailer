57b4770341f77e1a2a3e4ab95bad1fc706af2731
nicholavila <nicholavila1@outlook.com>
2024-02-22 12:22:07 +0000

    update style for more action

--- app/audience/_components/segments-column.tsx
+++ app/audience/_components/segments-column.tsx
@@ -101,27 +101,29 @@ export const getColumnsForSegmentsTable = ({
       cell: ({ row }) => {
         const segment = row.original;
         return (
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button
-                disabled={isPending}
-                variant="ghost"
-                className="h-8 w-8 p-0"
-              >
-                <span className="sr-only">Open menu</span>
-                <MoreHorizontal className="h-4 w-4" />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuLabel>Actions</DropdownMenuLabel>
-              <DropdownMenuItem>Copy Segment Info</DropdownMenuItem>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem onClick={() => onSegmentDelete(segment)}>
-                Delete Segment
-              </DropdownMenuItem>
-              <DropdownMenuItem>Edit Segment</DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+          <div className="flex justify-center">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  disabled={isPending}
+                  variant="ghost"
+                  className="h-8 w-8 p-0"
+                >
+                  <span className="sr-only">Open menu</span>
+                  <MoreHorizontal className="h-4 w-4" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                <DropdownMenuItem>Copy Segment Info</DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem onClick={() => onSegmentDelete(segment)}>
+                  Delete Segment
+                </DropdownMenuItem>
+                <DropdownMenuItem>Edit Segment</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
         );
       }
     }
