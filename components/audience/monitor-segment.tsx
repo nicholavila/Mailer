a4152bf292f9f7711d73e9af2b29656598c750dd
nicholavila <nicholavila1@outlook.com>
2024-02-26 16:00:27 +0000

    update monitor segment

--- components/audience/monitor-segment.tsx
+++ components/audience/monitor-segment.tsx
@@ -1,66 +1,44 @@
-import { FaCheck, FaFilter, FaPlus } from "react-icons/fa";
+import { FaCheck } from "react-icons/fa";
 import { Button } from "../ui/button";
 import { FilterBar } from "./filter-bar";
 import { useEffect, useState } from "react";
 import { getUserByEmail } from "@/data/user/user-by-email";
 import { useCurrentUser } from "@/hooks/use-current-user";
-import { FilterType } from "@/shared/filter-type";
-import { FormError } from "../utils/form-error";
-import { Input } from "../ui/input";
-import { Segment, SegmentAddition } from "@/shared/segment-type";
+import { Segment } from "@/shared/segment-type";
 
 type Props = {
   segment: Segment;
+  onFinish: () => void;
 };
 
-const MonitorSegment = ({ segment }: Props) => {
+const MonitorSegment = ({ segment, onFinish }: Props) => {
   const user = useCurrentUser();
+  const [storedTags, setStoredTags] = useState<string[]>([]);
 
-  const [filters, setFilters] = useState<FilterType[]>(segment.filters);
-  const [title, setTitle] = useState<string>(segment.title);
-  const [description, setDescription] = useState<string>(segment.description);
+  useEffect(() => {
+    getUserByEmail(user?.email as string).then((res) => {
+      if (res && res.tags) {
+        setStoredTags(res.tags);
+      }
+    });
+  }, []);
 
   return (
     <main className="w-full flex flex-col gap-y-4">
-      <p className="text-xl font-semibold">Edit Segment: {title}</p>
-      <div className="flex items-center gap-2">
-        <FaFilter />
-        Segment Filters
-      </div>
+      <p className="text-xl font-semibold">{segment.title}</p>
+      <div className="flex items-center gap-2">{segment.description}</div>
       <div className="flex flex-col gap-y-4">
-        {filters.map((filter, index) => (
-          <FilterBar filter={filter} storedTags={storedTags} />
+        {segment.filters.map((filter, index) => (
+          <FilterBar disabled filter={filter} storedTags={storedTags} />
         ))}
       </div>
-      <FormError message={error} />
-      <div className="flex items-center justify-between mt-2">
-        <Button
-          variant={"outline"}
-          className="w-48 flex items-center gap-x-2 border-green-700"
-        >
-          <FaPlus />
-          Add Filter
-        </Button>
-        <div className="flex items-center gap-x-4">
-          <p>Title</p>
-          <Input
-            value={title}
-            onChange={(event) => setTitle(event.target.value)}
-          />
-          <p>Description</p>
-          <Input
-            value={description}
-            onChange={(event) => setDescription(event.target.value)}
-          />
-          <Button
-            onClick={onSaveSegment}
-            className="min-w-48 flex items-center gap-x-2 border-green-700"
-          >
-            <FaCheck />
-            Save
-          </Button>
-        </div>
-      </div>
+      <Button
+        onClick={onFinish}
+        className="w-48 flex items-center self-end gap-x-2 border-green-700"
+      >
+        <FaCheck />
+        OK
+      </Button>
     </main>
   );
 };
