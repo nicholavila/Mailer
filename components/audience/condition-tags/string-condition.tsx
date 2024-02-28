ee70236b8114a6cc97423002827a8e537f0245fc
nicholavila <nicholavila1@outlook.com>
2024-02-28 12:57:08 +0000

    update string condition bar

--- components/audience/condition-tags/string-condition.tsx
+++ components/audience/condition-tags/string-condition.tsx
@@ -4,26 +4,31 @@ import {
   SelectContent,
   SelectGroup,
   SelectItem,
-  SelectLabel,
   SelectTrigger,
   SelectValue
 } from "@/components/ui/select";
-import { FilterType } from "@/shared/filter-type";
+import { Filter } from "@/shared/filter-type";
 
 type PropsType = {
-  filter: FilterType;
-  onConditionChange: (value: string) => void;
-  onValueChange: (value: string) => void;
+  disabled?: boolean;
+  filter: Filter;
+  onConditionChange?: (value: string) => void;
+  onValueChange?: (value: string) => void;
 };
 
 export const StringCondition = ({
+  disabled,
   filter,
   onConditionChange,
   onValueChange
 }: PropsType) => {
   return (
     <div className="flex items-center gap-x-6">
-      <Select value={filter.condition} onValueChange={onConditionChange}>
+      <Select
+        disabled={disabled}
+        value={filter.condition}
+        onValueChange={onConditionChange}
+      >
         <SelectTrigger className="w-[240px] px-4 bg-gray-200 rounded-full">
           <SelectValue placeholder="Select a condition" />
         </SelectTrigger>
@@ -36,10 +41,15 @@ export const StringCondition = ({
         </SelectContent>
       </Select>
       <Input
+        disabled={disabled}
         placeholder="Enter a value"
         className="w-[240px] px-4 rounded-full"
         value={filter.value}
-        onChange={(event) => onValueChange(event.target.value)}
+        onChange={
+          onValueChange
+            ? (event) => onValueChange(event.target.value)
+            : undefined
+        }
       />
     </div>
   );
