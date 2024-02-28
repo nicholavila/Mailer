05a0277eb74c22172d965d0ecb250fabac846ae3
nicholavila <nicholavila1@outlook.com>
2024-02-28 15:48:35 +0000

    update filter function

--- lib/segment.ts
+++ lib/segment.ts
@@ -1,5 +1,10 @@
 import { Filter } from "@/shared/filter-type";
-import { isTypeOfString, isTypeOfSubscribed } from "./filter-customer";
+import {
+  isTypeOfDate,
+  isTypeOfString,
+  isTypeOfSubscribed,
+  isTypeOfTags
+} from "./filter-customer";
 
 interface Customer {
   [key: string]: any;
@@ -8,6 +13,23 @@ interface Customer {
 const isFiltered = (customer: Customer, filters: Filter[]) => {
   for (let i = 0; i < filters.length; i++) {
     const filter = filters[i];
+    if (isTypeOfTags(filter.attribute)) {
+      if (
+        filter.condition === "contains" &&
+        !customer.tags.find((tag: string) => tag === filter.value)
+      )
+        return false;
+      else if (
+        filter.condition === "not-contains" &&
+        customer.tags.find((tag: string) => tag === filter.value)
+      )
+        return false;
+    } else if (isTypeOfSubscribed(filter.attribute)) {
+      if (customer.subscribed !== filter.value) {
+        return false;
+      }
+    } else if (isTypeOfDate(filter.attribute)) {
+    }
     if (isTypeOfString(filter.attribute)) {
       if (
         filter.condition === "contains" &&
@@ -19,11 +41,7 @@ const isFiltered = (customer: Customer, filters: Filter[]) => {
         customer[filter.attribute].includes(filter.value)
       )
         return false;
-    } else if (isTypeOfSubscribed(filter.attribute) {
-      if(customer.subscribed !== filter.value) {
-        
-      }
-    })
+    }
   }
   return true;
 };
