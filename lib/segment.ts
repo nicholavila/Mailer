0a011208626bcea0cf1131e1c6a3dbe35df1c774
nicholavila <nicholavila1@outlook.com>
2024-02-28 13:52:00 +0000

    feat: filter check function

--- lib/segment.ts
+++ lib/segment.ts
@@ -1,4 +1,25 @@
-import { Customer } from "@/shared/customer-type";
 import { Filter } from "@/shared/filter-type";
+import { isTypeOfString } from "./filter-customer";
 
-const isFiltered = (customer: Customer, filter: Filter[]) => {};
+interface Customer {
+  [key: string]: any;
+}
+
+const isFiltered = (customer: Customer, filters: Filter[]) => {
+  for (let i = 0; i < filters.length; i++) {
+    const filter = filters[i];
+    if (isTypeOfString(filter.attribute)) {
+      if (
+        filter.condition === "contains" &&
+        customer[filter.attribute].includes(filter.value)
+      )
+        return true;
+      else if (
+        filter.condition === "not-contains" &&
+        !customer[filter.attribute].includes(filter.value)
+      )
+        return true;
+    }
+  }
+  return false;
+};
