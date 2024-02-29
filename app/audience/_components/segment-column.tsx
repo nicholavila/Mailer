2e98acca251057f82b37db72f8f1a6f53b980431
nicholavila <nicholavila1@outlook.com>
2024-02-29 13:59:44 +0000

    refactor: actions for segment-column

--- app/audience/_components/segment-column.tsx
+++ app/audience/_components/segment-column.tsx
@@ -149,44 +149,6 @@ export const getColumnsForContactsTable = () => {
           : "";
         return <div className="text-center font-medium">{cellValue}</div>;
       }
-    },
-    {
-      id: "actions",
-      enableHiding: false,
-      cell: ({ row }) => {
-        const customer = row.original;
-        return (
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button
-                disabled={isPending}
-                variant="ghost"
-                className="h-8 w-8 p-0"
-              >
-                <span className="sr-only">Open menu</span>
-                <MoreHorizontal className="h-4 w-4" />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuLabel>Actions</DropdownMenuLabel>
-              <DropdownMenuItem
-                onClick={() =>
-                  navigator.clipboard.writeText(customer.subscriberEmail)
-                }
-              >
-                Copy Customer Email
-              </DropdownMenuItem>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem onClick={() => onCustomerDelete(customer)}>
-                Delete Customer
-              </DropdownMenuItem>
-              <DropdownMenuItem onClick={() => onCustomerEdit(customer)}>
-                Edit Customer
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        );
-      }
     }
   ];
 
