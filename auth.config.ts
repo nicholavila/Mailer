f8d3e3d200bac15295e9ac230fd232bbcd1b9641
nicholavila <nicholavila1@outlook.com>
2024-02-29 12:18:31 +0000

    refactor: add secret into auth config

--- auth.config.ts
+++ auth.config.ts
@@ -9,6 +9,7 @@ import type { NextAuthConfig } from "next-auth";
 import { getUserByEmail } from "@/data/user/user-by-email";
 
 export default {
+  secret: process.env.AUTH_SECRET,
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
