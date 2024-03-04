0fbef7723c429307d4af4af432ebc11ac2198335
nicholavila <nicholavila1@outlook.com>
2024-03-04 08:55:20 +0000

    update path for create-email

--- app/campaign/create-email/page.tsx
+++ app/campaign/create-email/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { DefaultJsonData } from "@/shared/default-email-json";
+import { DefaultJsonData } from "@/assets/default-email-json";
 import { useEffect, useRef, useState } from "react";
 import EmailEditor, { EditorRef, EmailEditorProps } from "react-email-editor";
 
