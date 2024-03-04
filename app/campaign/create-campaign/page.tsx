3def28fed665902cf69d3c2051e907d314408669
nicholavila <nicholavila1@outlook.com>
2024-03-04 11:19:07 +0000

    add form for campaign title

--- app/campaign/create-campaign/page.tsx
+++ app/campaign/create-campaign/page.tsx
@@ -5,40 +5,85 @@ import { FaArrowRight } from "react-icons/fa";
 import { Input } from "@/components/ui/input";
 import { z } from "zod";
 import { useState, useTransition } from "react";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const NewCampaignSchema = z.object({
   title: z
     .string()
     .min(6, "Email Title should be at least 6 characters long")
-    .max(72, "Email Title should be a maximum of 72 characters");
+    .max(72, "Email Title should be a maximum of 72 characters")
 });
 
 const NewCampaign = () => {
   const [error, setError] = useState<string | undefined>("");
-  const [isPending, startTransition] =useTransition();
+  const [isPending, startTransition] = useTransition();
 
-  const onNewCampaign = () => {};
+  const form = useForm<z.infer<typeof NewCampaignSchema>>({
+    resolver: zodResolver(NewCampaignSchema),
+    defaultValues: {
+      title: ""
+    }
+  });
+
+  const onSubmit = (values: z.infer<typeof NewCampaignSchema>) => {
+    setError("");
+
+    startTransition(() => {
+      // create campaign
+    });
+  };
 
   return (
-    <main className="w-5/6 flex flex-col py-6">
+    <div className="w-5/6 flex flex-col py-6">
       <p className="text-5xl text-green-700 font-semibold mb-4">
         Create a new Campaign
       </p>
       <p className="text-xl text-gray-500 mb-12">
         Reach your customer's inboxes at the right time with Campaign Manager.
       </p>
-      <p className="text-2xl font-semibold pb-2">Internet email name</p>
-      <Input type="text" className="w-96 h-12 border border-green-700 mb-4" />
-      <Button
-        asChild
-        variant="default"
-        className="w-40 flex gap-x-2"
-        onClick={onNewCampaign}
-      >
-        <FaArrowRight />
-        Create Email
-      </Button>
-    </main>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="mb-4">
+          <div className="space-y-2">
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Internet Email Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      disabled={isPending}
+                      className="w096 h-12 border border-green-700"
+                      placeholder="John Doe"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button
+            asChild
+            variant="default"
+            type="submit"
+            className="w-40 flex gap-x-2"
+          >
+            <FaArrowRight />
+            Create Email
+          </Button>
+        </form>
+      </Form>
+    </div>
   );
 };
 
