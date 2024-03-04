dc289b98cda9395e61c3efac836978f2d7beb8cc
nicholavila <nicholavila1@outlook.com>
2024-03-04 14:36:09 +0000

    rearrange: add edit-campaign page

---
+++ app/campaign/edit-campaign/[campaignId]/page.tsx
@@ -0,0 +1,11 @@
+type Props = {
+  campaignId: string;
+};
+
+const EditCampaignPage = ({ campaignId }: Props) => {
+  return (
+    <div>
+      <h1>Edit Campaign: {campaignId}</h1>
+    </div>
+  );
+};
