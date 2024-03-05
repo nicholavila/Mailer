d9d833166ed98fa8a895c0091311ed0e2fe8fd20
nicholavila <nicholavila1@outlook.com>
2024-03-05 14:41:09 +0000

    feat: add from inputs

--- app/campaign/edit-campaign/[campaignId]/page.tsx
+++ app/campaign/edit-campaign/[campaignId]/page.tsx
@@ -72,8 +72,8 @@ const EditCampaignPage = ({ params: { campaignId } }: Props) => {
       <p className="text-4xl text-green-700 font-semibold">
         Campaign {campaign?.title}
       </p>
-      <Accordion type="single" collapsible>
-        <AccordionItem value="item-1">
+      <Accordion type={"multiple"}>
+        <AccordionItem value="item-to">
           <AccordionTrigger className="hover:no-underline hover:drop-shadow">
             <div className="flex items-start gap-x-4">
               <div
@@ -85,6 +85,45 @@ const EditCampaignPage = ({ params: { campaignId } }: Props) => {
                 <p className="text-xl">To</p>
                 <p className="text-sm text-gray-500">
                   Who are you sending this email to?
+                </p>
+              </div>
+            </div>
+          </AccordionTrigger>
+          <AccordionContent className="px-1 pt-1">
+            <div className="flex flex-col gap-y-2">
+              <p className="text-base text-gray-600 font-medium">
+                Select a segment
+              </p>
+              <Select value={campaign?.to} onValueChange={onToChange}>
+                <SelectTrigger>
+                  <SelectValue placeholder="Segment you want to send" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Your Segments</SelectLabel>
+                    {segments.map((segment) => (
+                      <SelectItem value={segment.segmentId}>
+                        {segment.title}
+                      </SelectItem>
+                    ))}
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-from">
+          <AccordionTrigger className="hover:no-underline hover:drop-shadow">
+            <div className="flex items-start gap-x-4">
+              <div
+                className={`w-10 h-10 flex items-center justify-center rounded-full ${campaign?.from ? "bg-green-600" : "bg-gray-600"}`}
+              >
+                <FaCheck className="text-white" />
+              </div>
+              <div className="flex flex-col items-start">
+                <p className="text-xl">From</p>
+                <p className="text-sm text-gray-500">
+                  Who is sending this email?
                 </p>
               </div>
             </div>
