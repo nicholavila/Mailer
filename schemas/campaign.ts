82e8c599fc0cf8575e50bf6e36360e66ce1d813d
nicholavila <nicholavila1@outlook.com>
2024-03-07 09:03:18 +0000

    feat: add campaign subject schema

--- schemas/campaign.ts
+++ schemas/campaign.ts
@@ -7,3 +7,14 @@ export const CampaignFromSchema = z.object({
     .max(32, "Name should be a maximum of 32 characters"),
   email: z.string().email({ message: "Please enter a valid email" })
 });
+
+export const CampaignSubjectSchema = z.object({
+  subject: z
+    .string()
+    .min(6, "Subject should be at least 6 characters long")
+    .max(72, "Subject should be a maximum of 72 characters"),
+  preview: z
+    .string()
+    .min(6, "Preview should be at least 6 characters long")
+    .max(72, "Preview should be a maximum of 72 characters")
+});
