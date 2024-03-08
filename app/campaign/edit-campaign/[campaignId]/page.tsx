2b8be93b1e79037c03499b674c1d96673ee4a96a
nicholavila <nicholavila1@outlook.com>
2024-03-08 10:56:35 +0000

    refactor campaign edit page

--- app/campaign/edit-campaign/[campaignId]/page.tsx
+++ app/campaign/edit-campaign/[campaignId]/page.tsx
@@ -58,26 +58,6 @@ const EditCampaignPage = ({ params: { campaignId } }: Props) => {
     });
   }, []);
 
-  const subjectForm = useForm<z.infer<typeof CampaignSubjectSchema>>({
-    resolver: zodResolver(CampaignSubjectSchema),
-    defaultValues: {
-      subject: campaign?.subject?.subject,
-      preview: campaign?.subject?.preview
-    }
-  });
-
-  const onSubjectSubmit = (values: z.infer<typeof CampaignSubjectSchema>) => {
-    if (campaign) {
-      setCampaign(
-        (prev) =>
-          ({
-            ...prev,
-            subject: values
-          }) as Campaign
-      );
-    }
-  };
-
   if (loadError) {
     return notFound();
   }
@@ -101,74 +81,6 @@ const EditCampaignPage = ({ params: { campaignId } }: Props) => {
             setCampaign as React.Dispatch<React.SetStateAction<Campaign>>
           }
         />
-        <AccordionItem value="step-2-subject">
-          <AccordionTrigger className="hover:no-underline hover:drop-shadow">
-            <div className="flex items-start gap-x-4">
-              <div
-                className={`w-10 h-10 flex items-center justify-center rounded-full 
-                  ${campaign?.subject ? "bg-green-600" : "bg-gray-600"}
-                `}
-              >
-                <FaCheck className="text-white" />
-              </div>
-              <div className="flex flex-col items-start">
-                <p className="text-xl">Subject</p>
-                <p className="text-sm text-gray-500">
-                  What's the subject line for this email?
-                </p>
-              </div>
-            </div>
-          </AccordionTrigger>
-          <AccordionContent className="px-1 pt-1">
-            <Form {...subjectForm}>
-              <form
-                onSubmit={subjectForm.handleSubmit(onSubjectSubmit)}
-                className="w-full flex flex-col gap-y-4"
-              >
-                <div className="w-full flex items-start gap-x-6">
-                  <div className="w-1/2 flex">
-                    <FormField
-                      control={subjectForm.control}
-                      name="subject"
-                      render={({ field }) => (
-                        <FormItem className="w-full">
-                          <FormLabel>Subject*</FormLabel>
-                          <FormControl>
-                            <Input {...field} placeholder="Subject for email" />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                  </div>
-                  <div className="w-1/2 flex">
-                    <FormField
-                      control={subjectForm.control}
-                      name="preview"
-                      render={({ field }) => (
-                        <FormItem className="w-full">
-                          <FormLabel>Preview Text</FormLabel>
-                          <FormControl>
-                            <Input {...field} placeholder="Preview Text" />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                  </div>
-                </div>
-                <Button
-                  type="submit"
-                  variant="outline"
-                  className="w-48 flex items-center gap-x-2 border-green-700"
-                >
-                  <FaSave className="text-green-700" />
-                  Save
-                </Button>
-              </form>
-            </Form>
-          </AccordionContent>
-        </AccordionItem>
       </Accordion>
     </div>
   );
