b7f67e001e6c4ea9441cb1deaa2f216eb011d945
nicholavila <nicholavila1@outlook.com>
2024-03-13 13:50:56 +0000

    rearrange: update campaign

---
+++ data/campaign/update-campaign.ts
@@ -0,0 +1,38 @@
+"use server";
+
+import db from "@/lib/db";
+import { Campaign } from "@/shared/campaign-type";
+import { UpdateCommand } from "@aws-sdk/lib-dynamodb";
+
+const TableName = process.env.AWS_DYNAMODB_CAMPAIGNS_TABLE_NAME;
+
+export const updateCampaign = async (campaign: Campaign) => {
+  const command = new UpdateCommand({
+    TableName,
+    Key: { userEmail: campaign.userEmail, campaignId: campaign.campaignId },
+    UpdateExpression:
+      "SET title = :title, #to = :to, #from = :from, subject = :subject, #time = :time, emailContent = :emailContent",
+    ExpressionAttributeNames: {
+      "#to": "to",
+      "#from": "from",
+      "#time": "time"
+    },
+    ExpressionAttributeValues: {
+      ":title": campaign.title,
+      ":to": campaign.to || null,
+      ":from": campaign.from || null,
+      ":subject": campaign.subject || null,
+      ":time": campaign.time || null,
+      ":emailContent": campaign.emailContent || null
+    },
+    ReturnValues: "ALL_NEW"
+  });
+
+  try {
+    const response = await db.send(command);
+    return response.Attributes;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
