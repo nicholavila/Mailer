ecfeb03ce9ab41663e57b0a231a0e71b84f76c6c
nicholavila <nicholavila1@outlook.com>
2024-03-15 14:45:13 +0000

    feat: new func for running campaign

--- shared/funcs/runCampaign.ts
+++ shared/funcs/runCampaign.ts
@@ -3,6 +3,10 @@
 import { getSegmentById } from "@/data/segment/segment-by-id";
 import { Campaign } from "../campaign-type";
 import { Segment } from "../segment-type";
+import { getAllCustomersByEmail } from "@/data/audience/all-customers";
+import { Customer } from "../customer-type";
+import { isFiltered } from "@/lib/segment";
+import { transporter } from "@/lib/nodemailer";
 
 export const runCampaign = async (campaign: Campaign) => {
   const segmentId: string = campaign.to as string;
@@ -12,4 +16,21 @@ export const runCampaign = async (campaign: Campaign) => {
   )) as Segment;
 
   if (!segment) return;
+
+  const customers = (await getAllCustomersByEmail(
+    campaign.userEmail
+  )) as Customer[];
+
+  if (!customers) return;
+
+  customers.map((customer: Customer) => {
+    if (isFiltered(customer, segment.filters)) {
+      transporter.sendMail({
+        from: campaign.from?.email,
+        to: customer.subscriberEmail,
+        subject: campaign.subject?.subject,
+        html: campaign.email?.html
+      });
+    }
+  });
 };
