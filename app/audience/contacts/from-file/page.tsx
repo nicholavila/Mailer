7439024b246b6a89a9295e2a57623d4d13df45ff
nicholavila <nicholavila1@outlook.com>
2024-03-15 13:51:26 +0000

    feat: process text on from-file

--- app/audience/contacts/from-file/page.tsx
+++ app/audience/contacts/from-file/page.tsx
@@ -3,11 +3,43 @@
 import { Button } from "@/components/ui/button";
 import { FaArrowLeft, FaFileUpload } from "react-icons/fa";
 import Link from "next/link";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { Input } from "@/components/ui/input";
+import { recognizeLine } from "@/shared/funcs/recognizeLine";
+import { useCurrentUser } from "@/hooks/use-current-user";
+import { ConfirmAlert } from "@/components/utils/confirm-alert";
+import { useRouter } from "next/navigation";
 
 const FromFile = () => {
+  const user = useCurrentUser();
   const hiddenFileInput = useRef<HTMLInputElement>(null);
+  const history = useRouter();
+
+  const [isLoading, setLoading] = useState<boolean>(false);
+  const [isConfirming, setConfirming] = useState<boolean>(false);
+  const [confirmText, setConfirmText] = useState<string>("");
+
+  const processText = (text: string, splitter: string) => {
+    setLoading(true);
+
+    const lines = text.split("\n");
+    Promise.all(
+      lines.map(async (line) => {
+        const res = await recognizeLine(user?.email as string, line, splitter);
+        return res;
+      })
+    ).then((res) => {
+      const successedCnt = res.reduce(
+        (accumulator, cur) => accumulator + cur,
+        0
+      );
+      setLoading(false);
+      setConfirming(true);
+      setConfirmText(
+        `${successedCnt} contacts out of ${lines.length} were added successfully.`
+      );
+    });
+  };
 
   const onFileSelected = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
