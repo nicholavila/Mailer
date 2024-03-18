e0d9e8ee0f3c2dd4d92c639ff1d32a2c80261713
nicholavila <nicholavila1@outlook.com>
2024-03-18 10:49:27 +0000

    feat: remove campaign feature

--- components/campaign/campaign-item.tsx
+++ components/campaign/campaign-item.tsx
@@ -2,47 +2,106 @@ import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Campaign } from "@/shared/campaign-type";
 import Link from "next/link";
+import { useEffect, useState, useTransition } from "react";
 import { FaEdit, FaMailBulk, FaRemoveFormat, FaTrash } from "react-icons/fa";
+import { ConfirmAlert } from "../utils/confirm-alert";
+import { useCurrentUser } from "@/hooks/use-current-user";
+import { deleteCampaign } from "@/data/campaign/delete-campaign";
+import { toast } from "sonner";
+import { Spinner } from "@nextui-org/spinner";
+import { Segment } from "@/shared/segment-type";
+import { getSegmentById } from "@/data/segment/segment-by-id";
 
 type Props = {
   campaign: Campaign;
+  onRemove: () => void;
 };
 
-const CampaignItem = ({ campaign }: Props) => {
+const CampaignItem = ({ campaign, onRemove }: Props) => {
+  const user = useCurrentUser();
+
+  const [isPending, startTransition] = useTransition();
+  const [confirmAlert, setConfirmAlert] = useState(false);
+  const [segment, setSegment] = useState<Segment>();
+
+  useEffect(() => {
+    if (campaign.to) {
+      getSegmentById(user?.email as string, campaign.to).then((response) => {
+        if (response) {
+          setSegment(response as Segment);
+        }
+      });
+    }
+  }, []);
+
+  const onConfirmAlertClosed = () => {
+    // # Need to Stop Trigger? #
+    setConfirmAlert(false);
+    startTransition(() => {
+      deleteCampaign(user?.email as string, campaign.campaignId)
+        .then((response) => {
+          if (response.success) {
+            onRemove();
+          } else {
+            toast.error("Failed to remove campaign");
+          }
+        })
+        .catch(() => {
+          toast.error("Failed to remove campaign");
+        });
+    });
+  };
+
   return (
-    <div className="w-full flex justify-between">
-      <div className="flex gap-x-8">
+    <div className="w-full flex gap-x-6">
+      <ConfirmAlert
+        open={confirmAlert}
+        title="Remove Campaign"
+        description="Are you sure you want to remove this campaign?"
+        onAlertDialogClosed={onConfirmAlertClosed}
+      />
+      <div className="w-3/6 flex gap-x-8">
         <FaMailBulk className="text-4xl text-yellow-700" />
         <div className="flex flex-col gap-y-0 text-gray-500">
           <p className="text-xl text-blue-500">{campaign.title}</p>
           <p className="text-base">Subject · {campaign.subject?.subject}</p>
-          <p className="text-base">To · {campaign.to}</p>
+          <p className="text-base">
+            To ·{" "}
+            <span className="text-green-700 font-semi-bold">
+              {segment?.title}
+            </span>
+          </p>
           <p className="text-base">Edited · </p>
         </div>
       </div>
-      <div className="flex flex-col gap-y-2">
+      <div className="w-2/6 flex flex-col items-center gap-y-2">
         <Badge
           variant={"success"}
-          className="w-fit text-base font-normal text-black text-center"
+          className="w-fit text-sm font-normal text-black text-center"
         >
           Draft
         </Badge>
-        <Badge className="text-base font-normal text-black text-center">
+        <Badge className="text-sm font-normal text-black text-center">
           New Builder
         </Badge>
       </div>
-      <div className="flex flex-col gap-y-2">
-        <Button asChild className="flex gap-x-2 w-32 text-black bg-green-500">
-          <Link href={`/campaign/edit-campaign/${campaign.campaignId}`}>
+      <div className="w-1/6 flex flex-col items-center gap-y-2">
+        <Button disabled={isPending} className="w-fit flex">
+          <Link
+            href={`/campaign/edit-campaign/${campaign.campaignId}`}
+            className="flex items-center gap-x-2 text-black"
+          >
             <FaEdit />
             Edit
           </Link>
         </Button>
         <Button
+          disabled={isPending}
           variant={"destructive"}
-          className="flex gap-x-2 w-32 text-black"
+          className="w-fit flex gap-x-2 text-black"
+          onClick={() => setConfirmAlert(true)}
         >
-          <FaTrash />
+          {isPending ? <Spinner size="sm" /> : <FaTrash />}
           Remove
         </Button>
       </div>
