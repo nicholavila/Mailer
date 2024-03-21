3b55515fbca875c25341501d15ba05edc0b43d02
nicholavila <nicholavila1@outlook.com>
2024-03-21 09:33:03 +0000

    rearrange: update subscribers-all with prisma

---
+++ data/audience/subscribers-all.ts
@@ -0,0 +1,37 @@
+"use server";
+
+import { prisma } from "@/lib/prisma";
+
+// import db from "@/lib/db";
+// import { QueryCommand } from "@aws-sdk/lib-dynamodb";
+
+// const TableName = process.env.AWS_DYNAMODB_MAILING_LIST_TABLE_NAME;
+
+// export const getAllSubscribersByEmail = async (userEmail: string) => {
+//   const command = new QueryCommand({
+//     TableName,
+//     KeyConditionExpression: "userEmail = :userEmail",
+//     ExpressionAttributeValues: {
+//       ":userEmail": userEmail
+//     }
+//   });
+
+//   try {
+//     const response = await db.send(command);
+//     return response.Items;
+//   } catch (error) {
+//     return null;
+//   }
+// };
+
+export const getAllSubscribersByEmail = async (userEmail: string) => {
+  try {
+    return await prisma.mailinglist.findMany({
+      where: {
+        userEmail
+      }
+    });
+  } catch (error) {
+    return null;
+  }
+};
