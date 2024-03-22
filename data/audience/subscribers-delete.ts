50a4d415e6dbb4f1defdbdf9156bbcdc6d07b219
nicholavila <nicholavila1@outlook.com>
2024-03-22 08:35:29 +0000

    feat: delete customers with prisma

---
+++ data/audience/subscribers-delete.ts
@@ -0,0 +1,47 @@
+"use server";
+
+import { prisma } from "@/lib/prisma";
+
+// import db from "@/lib/db";
+// import { DeleteCommand } from "@aws-sdk/lib-dynamodb";
+
+// const TableName = process.env.AWS_DYNAMODB_MAILING_LIST_TABLE_NAME;
+
+// export const deleteSubscribers = async (
+//   userEmail: string,
+//   subscriberEmails: string[]
+// ) => {
+//   try {
+//     const response = await Promise.all(
+//       subscriberEmails.map(async (subscriberEmail) => {
+//         const command = new DeleteCommand({
+//           TableName,
+//           Key: { userEmail, subscriberEmail }
+//         });
+//         await db.send(command);
+//       })
+//     );
+//     return { success: true };
+//   } catch (error) {
+//     return { success: false };
+//   }
+// };
+
+export const deleteSubscribers = async (
+  userEmail: string,
+  subscriberEmails: string[]
+) => {
+  try {
+    await prisma.mailinglist.deleteMany({
+      where: {
+        userEmail,
+        subscriberEmail: {
+          in: subscriberEmails
+        }
+      }
+    });
+    return { success: true };
+  } catch (error) {
+    return { success: false };
+  }
+};
