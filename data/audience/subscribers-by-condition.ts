6ae8bdd2cf4106053e964fa47f9f1168ca214bb0
nicholavila <nicholavila1@outlook.com>
2024-03-25 10:45:10 +0000

    update: get mailing list by condition with prisma

---
+++ data/audience/subscribers-by-condition.ts
@@ -0,0 +1,11 @@
+"use server";
+
+import { prisma } from "@/lib/prisma";
+
+export const getAllSubscribersByCondition = async (condition: any) => {
+  try {
+    return await prisma.mailinglist.findMany(condition);
+  } catch (error) {
+    return null;
+  }
+};
