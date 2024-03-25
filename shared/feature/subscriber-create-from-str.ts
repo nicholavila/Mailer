caeb7f578f32b3072df0bd831e62de59810af79e
nicholavila <nicholavila1@outlook.com>
2024-03-25 14:04:15 +0000

    feat: create subscriber from str

---
+++ shared/feature/subscriber-create-from-str.ts
@@ -0,0 +1,37 @@
+import { createSubscriber } from "@/data/audience/subscriber-create";
+import { Subscriber } from "../types/subscriber";
+
+type FunctionType = (
+  userEmail: string,
+  str: string,
+  splitter: string
+) => Promise<number>;
+
+export const createSubscriberFromStr: FunctionType = async (
+  userEmail,
+  str,
+  splitter
+) => {
+  const fields = str.split(splitter);
+  if (fields.length < 6) {
+    return 0;
+  }
+
+  const contact: Subscriber = {
+    userEmail,
+    subscriberEmail: fields[0].trim(),
+    firstName: fields[1].trim(),
+    lastName: fields[2].trim(),
+    address: fields[3],
+    phoneNumber: fields[4],
+    birthday: new Date(fields[5]),
+    tags: [],
+    subscribed: true,
+    contactRating: 0,
+    created: new Date(),
+    lastChanged: new Date()
+  };
+
+  const res = await createSubscriber(contact);
+  return res.success ? 1 : 0;
+};
