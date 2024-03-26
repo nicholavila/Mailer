a46436c5a377980e4ec675e293d5a60ec9442397
nicholavila <nicholavila1@outlook.com>
2024-03-26 15:18:06 +0000

    feat: get campaign by id

--- data/campaign/campaign-by-id.ts
+++ data/campaign/campaign-by-id.ts
@@ -1,22 +1,39 @@
 "use server";
 
-import db from "@/lib/db";
-import { GetCommand } from "@aws-sdk/lib-dynamodb";
+// import db from "@/lib/db";
+import { prisma } from "@/lib/prisma";
+// import { GetCommand } from "@aws-sdk/lib-dynamodb";
 
-const TableName = process.env.AWS_DYNAMODB_CAMPAIGNS_TABLE_NAME;
+// const TableName = process.env.AWS_DYNAMODB_CAMPAIGNS_TABLE_NAME;
+
+// export const getCampaignById = async (
+//   userEmail: string,
+//   campaignId: string
+// ) => {
+//   const command = new GetCommand({
+//     TableName,
+//     Key: { userEmail, campaignId }
+//   });
+
+//   try {
+//     const response = await db.send(command);
+//     return response.Item;
+//   } catch (error) {
+//     return null;
+//   }
+// };
 
 export const getCampaignById = async (
   userEmail: string,
   campaignId: string
 ) => {
-  const command = new GetCommand({
-    TableName,
-    Key: { userEmail, campaignId }
-  });
-
   try {
-    const response = await db.send(command);
-    return response.Item;
+    return await prisma.campaigns.findFirst({
+      where: {
+        userEmail: userEmail,
+        campaignId: campaignId
+      }
+    });
   } catch (error) {
     return null;
   }
