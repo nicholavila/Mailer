ab1d23fdb3d57f9a932b8fd2b9366ba918504437
nicholavila <nicholavila1@outlook.com>
2024-03-27 10:04:44 +0000

    feat: delete campaign by userEmail

--- data/campaign/delete-campaign.ts
+++ data/campaign/delete-campaign.ts
@@ -1,20 +1,34 @@
 "use server";
 
-import db from "@/lib/db";
-import { DeleteCommand } from "@aws-sdk/lib-dynamodb";
+// import db from "@/lib/db";
+import { prisma } from "@/lib/prisma";
+// import { DeleteCommand } from "@aws-sdk/lib-dynamodb";
 
-const TableName = process.env.AWS_DYNAMODB_CAMPAIGNS_TABLE_NAME;
+// const TableName = process.env.AWS_DYNAMODB_CAMPAIGNS_TABLE_NAME;
+
+// export const deleteCampaign = async (userEmail: string, campaignId: string) => {
+//   const command = new DeleteCommand({
+//     TableName,
+//     Key: { userEmail, campaignId }
+//   });
+
+//   try {
+//     const response = await db.send(command);
+//     return { success: true, response };
+//   } catch (error) {
+//     return { error };
+//   }
+// };
 
 export const deleteCampaign = async (userEmail: string, campaignId: string) => {
-  const command = new DeleteCommand({
-    TableName,
-    Key: { userEmail, campaignId }
-  });
-
   try {
-    const response = await db.send(command);
-    return { success: true, response };
+    await prisma.campaigns.deleteMany({
+      where: {
+        userEmail,
+        campaignId
+      }
+    });
   } catch (error) {
-    return { error };
+    return { success: false };
   }
 };
