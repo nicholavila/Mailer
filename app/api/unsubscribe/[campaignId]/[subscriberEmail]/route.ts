579ed9c7c38f3d520d0e9af9b5e8de7184ecc050
nicholavila <nicholavila1@outlook.com>
2024-03-28 14:40:03 +0000

    feat: unsubscribe api

--- app/api/unsubscribe/[campaignId]/[subscriberEmail]/route.ts
+++ app/api/unsubscribe/[campaignId]/[subscriberEmail]/route.ts
@@ -1,8 +1,21 @@
-import { NextRequest } from "next/server";
+import { getCampaignById } from "@/data/campaign/campaign-by-id";
+import { NextRequest, NextResponse } from "next/server";
 
 type Params = {
   campaignId: string;
   subscriberEmail: string;
 };
 
-export const POST = async (request: NextRequest, params: Params) => {};
+export const POST = async (
+  request: NextRequest,
+  response: NextResponse,
+  params: Params
+) => {
+  console.log(params);
+  const { campaignId, subscriberEmail } = params;
+  const campaign = await getCampaignById(campaignId);
+  if (campaign === null) {
+    return NextResponse.json({ error: "Campaign not found" }, { status: 404 });
+  }
+  return NextResponse.json({ success: true });
+};
