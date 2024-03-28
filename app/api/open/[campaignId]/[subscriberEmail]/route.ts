23a2b80fdc5dc30f2804eb7bc666343568ee94b6
nicholavila <nicholavila1@outlook.com>
2024-03-28 13:35:25 +0000

    fix issue on campaignId

--- app/api/open/[campaignId]/[subscriberEmail]/route.ts
+++ app/api/open/[campaignId]/[subscriberEmail]/route.ts
@@ -1,6 +1,6 @@
 import { getCampaignById } from "@/data/campaign/campaign-by-id";
 import { updateCampaignOpened } from "@/data/campaign/campaign-update-opened";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 type Params = {
   campaignId: string;
@@ -8,13 +8,21 @@ type Params = {
 };
 
 // ## http://localhost:3000/api/open/id/mail
-export const POST = async (request: NextRequest, params: Params) => {
+export const POST = async (
+  request: NextRequest,
+  response: NextResponse,
+  params: Params
+) => {
+  console.log(params);
   const { campaignId, subscriberEmail } = params;
   const campaign = await getCampaignById(campaignId);
   if (campaign === null) {
-    return;
+    return NextResponse.json({ error: "Campaign not found" }, { status: 404 });
   }
+
   const _openedEmails = campaign.openedEmails || [];
   const openedEmails = [..._openedEmails, subscriberEmail];
   await updateCampaignOpened(campaignId, openedEmails);
+
+  return NextResponse.json({ success: true });
 };
