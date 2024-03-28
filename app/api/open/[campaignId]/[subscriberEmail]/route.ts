c3a8dfedaff38f1fa71f6eadf7b4abf64089ddd4
nicholavila <nicholavila1@outlook.com>
2024-03-28 10:40:42 +0000

    doc: add doc for correct api request

--- app/api/open/[campaignId]/[subscriberEmail]/route.ts
+++ app/api/open/[campaignId]/[subscriberEmail]/route.ts
@@ -5,6 +5,7 @@ type Params = {
   subscriberEmail: string;
 };
 
+// ## http://localhost:3000/api/open/id/mail
 export const POST = async (request: NextRequest, params: Params) => {
   console.log("Request", request);
   console.log("Params", params);
