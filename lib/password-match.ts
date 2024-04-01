4b662e68f002e2814498b77f8108af92603c5d2a
nicholavila <nicholavila1@outlook.com>
2024-04-01 09:55:18 +0000

    update module with issues

--- lib/password-match.ts
+++ lib/password-match.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 
 export const passwordsMatch = async (password: string, hash: string) => {
   const _match = await bcrypt.compare(password, hash);
