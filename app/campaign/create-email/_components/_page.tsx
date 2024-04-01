294eac2147eee7472dfc93e712f6c8b92ec3c810
nicholavila <nicholavila1@outlook.com>
2024-04-01 10:18:31 +0000

    rearrange create email page

---
+++ app/campaign/create-email/_components/_page.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { DEFAULT_EMAIL_DESIGN } from "@/assets/default-email-json";
+import { Button } from "@/components/ui/button";
+import { savedCampaignAtom } from "@/store/saved-campaign-atom";
+import { savedEmailAtom } from "@/store/saved-email-atom";
+import { useAtom } from "jotai";
+import { useRouter } from "next/navigation";
+import { useRef, useState } from "react";
+import EmailEditor, { EditorRef, EmailEditorProps } from "react-email-editor";
+
+export const NewEmail = () => {
+  const history = useRouter();
+
+  const [isLoading, setLoading] = useState(true);
+  const [savedEmail, setSavedEmail] = useAtom(savedEmailAtom);
+  const [savedCampaign] = useAtom(savedCampaignAtom);
+
+  const emailEditorRef = useRef<EditorRef>(null);
+
+  const onReady: EmailEditorProps["onReady"] = () => {
+    const unlayer = emailEditorRef.current?.editor;
+    unlayer?.loadDesign(
+      savedEmail.isSaved ? savedEmail.email.design : DEFAULT_EMAIL_DESIGN
+    );
+    setLoading(false);
+  };
+
+  const onSaveExist = () => {
+    const unlayer = emailEditorRef.current?.editor;
+    unlayer?.exportHtml(async (data) => {
+      const { design, html } = data;
+      setSavedEmail({
+        isSaved: true,
+        email: { design, html }
+      });
+      history.push(
+        `/campaign/edit-campaign/${savedCampaign.campaign.campaignId}`
+      );
+    });
+  };
+
+  return (
+    <div className="w-5/6 flex flex-col py-6">
+      <div className="w-full flex items-end justify-between pb-4">
+        <p className="text-3xl text-green-700 font-semibold">
+          Create a new Email
+        </p>
+        <Button disabled={isLoading} onClick={onSaveExist}>
+          Save & Exist
+        </Button>
+      </div>
+      <EmailEditor minHeight={"80vh"} ref={emailEditorRef} onReady={onReady} />
+    </div>
+  );
+};
+
+export default NewEmail;
