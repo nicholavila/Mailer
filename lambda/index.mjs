2b807addc608d0ca7898a6010f5a6968451e6746
nicholavila <nicholavila1@outlook.com>
2024-04-02 11:44:06 +0000

    feat: lambda function

--- lambda/index.mjs
+++ lambda/index.mjs
@@ -1,37 +1,28 @@
-import axios from "axios";
 import { SESClient } from "@aws-sdk/client-ses";
 import { SendEmailCommand } from "@aws-sdk/client-ses";
 
-const url_api_get_campaign = "https://mailmanjs-git.vercel.app/api/campaign/";
+import axios from "axios";
+
+const url_api_campaign = "https://mailmanjs-git.vercel.app/api/campaign/";
+const url_api_unsub = "https://mailmanjs-git.vercel.app/unsubscribe/";
 
 export const handler = async (event, context) => {
   const campaignId = event.campaignId;
-  const url = url_api_get_campaign + campaignId;
+  const url_campaign = url_api_campaign + campaignId;
 
-  const response = await axios.get(url);
+  const response = await axios.get(url_campaign);
   if (!response) {
     // Set failed flag on db
   }
 
   const campaign = response.data;
-  console.log(campaign);
+
+  const from = campaign.from;
+  const customers = campaign.customers;
+  const subject = campaign.subject;
+  const html = campaign.html;
 
   const sesClient = new SESClient();
-  const command = new SendEmailCommand({
-    Source: "malachi.uudev@gmail.com",
-    Destination: {
-      ToAddresses: ["malachi.uudev@gmail.com"]
-    },
-    Message: {
-      Subject: {
-        Data: "Hi There!"
-      },
-      Body: {
-        Html: {
-          Data: "<p>Hi There!</p>"
-        }
-      }
-    }
-  });
-  const res = await sesClient.send(command);
+
+  // for (let i = 0; i < customers.length; i++) {
 };
