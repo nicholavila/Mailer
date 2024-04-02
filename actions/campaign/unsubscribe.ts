c063bfe0cfdab1c9b288c2aea63440806924d2b3
nicholavila <nicholavila1@outlook.com>
2024-04-02 11:09:11 +0000

    rearrange: unsubscribe api

---
+++ actions/campaign/unsubscribe.ts
@@ -0,0 +1,32 @@
+import { getCampaignById } from "@/data/campaign/campaign-by-id";
+import { updateCampaignUnsub } from "@/data/campaign/campaign-update-unsub";
+
+type Params = {
+  campaignId: string;
+  subscriberEmail: string;
+};
+
+export const unsubscribe = async (params: Params) => {
+  const { campaignId, subscriberEmail } = params;
+  const campaign = await getCampaignById(campaignId);
+  if (campaign === null) {
+    return { error: "Campaign not found" };
+  }
+
+  const _unsubEmails = campaign.unsubEmails || [];
+  const isAlreadyUnsubed = _unsubEmails.includes(subscriberEmail);
+  if (isAlreadyUnsubed) {
+    return {
+      error: "You have already unsubscribed from this campaign"
+    };
+  }
+
+  const unsubEmails = [..._unsubEmails, subscriberEmail];
+  const res = await updateCampaignUnsub(campaignId, unsubEmails);
+
+  if (res?.success) {
+    return { success: true };
+  } else {
+    return { error: "Failed to update campaign" };
+  }
+};
