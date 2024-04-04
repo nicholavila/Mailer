69a80b21556679976a0f8d0448eec5bdf77bffb6
nicholavila <nicholavila1@outlook.com>
2024-04-04 14:20:18 +0000

    update: run campaign

--- actions/campaign/run-campaign.ts
+++ actions/campaign/run-campaign.ts
@@ -1,9 +1,17 @@
+import lambdaClient from "@/lib/lambda";
 import { InvokeCommand } from "@aws-sdk/client-lambda";
 
-export const runCampaign = () => {
+export const runCampaign = (campaignId: string) => {
   const command = new InvokeCommand({
     FunctionName: "runCampaign",
     InvocationType: "Event",
-    Payload: JSON.stringify({ campaignId: "" })
+    Payload: JSON.stringify({ campaignId })
   });
+
+  try {
+    const res = lambdaClient.send(command);
+    console.log(res);
+  } catch (error) {
+    console.error(error);
+  }
 };
