7919b1e8da2f4eb3b6f390d85649c9bdf3394fcd
nicholavila <nicholavila1@outlook.com>
2024-04-05 14:24:29 +0000

    update campaign by id

--- data/campaign/campaign-by-id.ts
+++ data/campaign/campaign-by-id.ts
@@ -2,6 +2,7 @@
 
 // import db from "@/lib/db";
 import { prisma } from "@/lib/prisma";
+import { Campaign } from "@/shared/types/campaign";
 // import { GetCommand } from "@aws-sdk/lib-dynamodb";
 
 // const TableName = process.env.AWS_DYNAMODB_CAMPAIGNS_TABLE_NAME;
@@ -28,13 +29,18 @@ export const getCampaignById = async (
   campaignId: string
 ) => {
   try {
-    return await prisma.campaigns.findUnique({
+    const campaign: Campaign = (await prisma.campaigns.findUnique({
       where: {
         // userEmail: userEmail,
         campaignId: campaignId
       }
-    });
+    })) as unknown as Campaign;
+    if (campaign?.time?.date) {
+      campaign.time.date = new Date(campaign.time.date);
+    }
+    return campaign;
   } catch (error) {
+    console.error(error);
     return null;
   }
 };
