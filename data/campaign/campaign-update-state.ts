793ab1fb039ef0f1eef7d0307463872df457fc5d
nicholavila <nicholavila1@outlook.com>
2024-04-08 13:07:20 +0000

    rearrange: campaing udpate state

---
+++ data/campaign/campaign-update-state.ts
@@ -0,0 +1,24 @@
+"use server";
+
+import { prisma } from "@/lib/prisma";
+import { CampaignState } from "@/shared/types/campaign";
+
+export const updateCampaignState = async (
+  campaignId: string,
+  state: CampaignState
+) => {
+  try {
+    await prisma.campaigns.update({
+      where: {
+        campaignId
+      },
+      data: {
+        state
+      }
+    });
+
+    return { success: true };
+  } catch (error) {
+    return null;
+  }
+};
