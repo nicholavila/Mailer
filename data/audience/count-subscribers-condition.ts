d945e8773ab4b6ef50bd88b0bccb23c1cd6d4dd4
nicholavila <nicholavila1@outlook.com>
2024-04-08 12:54:34 +0000

    chore: clean up

--- data/audience/count-subscribers-condition.ts
+++ data/audience/count-subscribers-condition.ts
@@ -6,7 +6,6 @@ export const getNubmersOfSubscribersByCondition = async (condition: any) => {
   try {
     return await prisma.mailinglist.count(condition);
   } catch (error) {
-    console.log("Error", error);
     return null;
   }
 };
