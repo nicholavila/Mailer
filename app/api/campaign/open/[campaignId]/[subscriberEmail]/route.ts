80409bb76d08cf0a07744ab52f963feec556e9a5
nicholavila <nicholavila1@outlook.com>
2024-04-08 15:05:05 +0000

    feat: api for open image link

---
+++ app/api/campaign/open/[campaignId]/[subscriberEmail]/route.ts
@@ -0,0 +1,41 @@
+import { getCampaignById } from "@/data/campaign/campaign-by-id";
+import { updateCampaignOpened } from "@/data/campaign/campaign-update-opened";
+import { NextRequest, NextResponse } from "next/server";
+
+type Params = {
+  params: {
+    campaignId: string;
+    subscriberEmail: string;
+  };
+};
+
+// ## http://localhost:3000/api/open/campaign-id/user-email
+
+export const GET = async (request: NextRequest, { params }: Params) => {
+  const { campaignId, subscriberEmail } = params;
+  const campaign = await getCampaignById(campaignId);
+  if (campaign === null) {
+    return NextResponse.json({ error: "Campaign not found" }, { status: 404 });
+  }
+
+  const _openedEmails = campaign.openedEmails || [];
+  const isAlreadyOpened = _openedEmails.includes(subscriberEmail);
+  if (isAlreadyOpened) {
+    return NextResponse.json(
+      { error: "Email was already regitered as opened" },
+      { status: 404 }
+    );
+  }
+
+  const openedEmails = [..._openedEmails, subscriberEmail];
+  const res = await updateCampaignOpened(campaignId, openedEmails);
+
+  if (res?.success) {
+    return NextResponse.json({ success: true });
+  } else {
+    return NextResponse.json(
+      { error: "Failed to update campaign" },
+      { status: 500 }
+    );
+  }
+};
