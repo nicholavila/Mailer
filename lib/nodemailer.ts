25dab68b2f0fe0d66f37e9743cc3ff6778a7df5b
nicholavila <nicholavila1@outlook.com>
2024-04-10 10:49:15 +0000

    rearrange: remove mailing on resend module

--- lib/nodemailer.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-
-import * as nodemailer from "nodemailer";
-import sesClient from "./ses";
-
-// # Not working #
-export const transporter = nodemailer.createTransport({
-  SES: sesClient
-});
