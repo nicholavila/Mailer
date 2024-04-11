22f2b796f43b6f785d8928e6976a3b8b4a149486
nicholavila <nicholavila1@outlook.com>
2024-04-11 09:02:19 +0000

    feat: api for campaign unsubscribe

---
+++ app/api/campaign/unsubscribe/[campaignId]/[subscriberEmail]/route.ts
@@ -0,0 +1,41 @@
+import { getCampaignById } from "@/data/campaign/campaign-by-id";
+import { updateCampaignUnsub } from "@/data/campaign/campaign-update-unsub";
+import { NextRequest, NextResponse } from "next/server";
+
+type Params = {
+  params: {
+    campaignId: string;
+    subscriberEmail: string;
+  };
+};
+
+// ## http://localhost:3000/api/unsubscribe/campaign-id/user-email
+
+export const POST = async (request: NextRequest, { params }: Params) => {
+  const { campaignId, subscriberEmail } = params;
+  const campaign = await getCampaignById(campaignId);
+  if (campaign === null) {
+    return NextResponse.json({ error: "Campaign not found" }, { status: 404 });
+  }
+
+  const _unsubEmails = campaign.unsubEmails || [];
+  const isAlreadyUnsubed = _unsubEmails.includes(subscriberEmail);
+  if (isAlreadyUnsubed) {
+    return NextResponse.json(
+      { error: "Email was already regitered as unsubscribed" },
+      { status: 404 }
+    );
+  }
+
+  const unsubEmails = [..._unsubEmails, subscriberEmail];
+  const res = await updateCampaignUnsub(campaignId, unsubEmails);
+
+  if (res.success) {
+    return NextResponse.json({ success: true });
+  } else {
+    return NextResponse.json(
+      { error: "Failed to update campaign" },
+      { status: 500 }
+    );
+  }
+};
