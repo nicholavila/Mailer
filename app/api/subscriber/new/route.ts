6eb89c788aa062445a938d48260d7d58a266ce77
nicholavila <nicholavila1@outlook.com>
2024-04-12 13:53:52 +0000

    chore: clean up for cors

--- app/api/subscriber/new/route.ts
+++ app/api/subscriber/new/route.ts
@@ -1,32 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
-import bodyParser from "body-parser";
 
 // ## http://localhost:3000/api/subscriber/new
-
 export const POST = async (request: NextRequest) => {
-  console.log("POST /api/subscriber/new");
-  const data = request.body;
-  const jsonParser = bodyParser.json();
-  jsonParser(request, null);
-  console.log(_data);
+  const data = await request.json();
+  console.log(data);
 
-  // if (campaign === null) {
-  //   return NextResponse.json({ error: "Campaign not found" }, { status: 404 });
-  // }
-
-  // if (isAlreadyOpened) {
-  //   return NextResponse.json(
-  //     { error: "Email was already regitered as opened" },
-  //     { status: 404 }
-  //   );
-  // }
-
-  // if (res.success) {
-  //   return NextResponse.json({ success: true });
-  // } else {
-  return NextResponse.json(
-    { error: "Failed to update campaign" },
-    { status: 500 }
-  );
-  // }
+  return NextResponse.json({ message: "Subscriber added" });
 };
