eaa3d0f120addf6c992bdcc1b71dd43ce2e3cda2
nicholavila <nicholavila1@outlook.com>
2024-04-12 10:01:28 +0000

    update: sunbscriber new api

--- app/api/subscriber/new/route.ts
+++ app/api/subscriber/new/route.ts
@@ -1,30 +1,32 @@
-import { getCampaignById } from "@/data/campaign/campaign-by-id";
-import { updateCampaignOpened } from "@/data/campaign/campaign-update-opened";
 import { NextRequest, NextResponse } from "next/server";
+import bodyParser from "body-parser";
 
 // ## http://localhost:3000/api/subscriber/new
 
 export const POST = async (request: NextRequest) => {
-  const data = await request.json();
-  console.log(data);
+  console.log("POST /api/subscriber/new");
+  const data = request.body;
+  const jsonParser = bodyParser.json();
+  jsonParser(request, null);
+  console.log(_data);
 
-  if (campaign === null) {
-    return NextResponse.json({ error: "Campaign not found" }, { status: 404 });
-  }
+  // if (campaign === null) {
+  //   return NextResponse.json({ error: "Campaign not found" }, { status: 404 });
+  // }
 
-  if (isAlreadyOpened) {
-    return NextResponse.json(
-      { error: "Email was already regitered as opened" },
-      { status: 404 }
-    );
-  }
+  // if (isAlreadyOpened) {
+  //   return NextResponse.json(
+  //     { error: "Email was already regitered as opened" },
+  //     { status: 404 }
+  //   );
+  // }
 
-  if (res.success) {
-    return NextResponse.json({ success: true });
-  } else {
-    return NextResponse.json(
-      { error: "Failed to update campaign" },
-      { status: 500 }
-    );
-  }
+  // if (res.success) {
+  //   return NextResponse.json({ success: true });
+  // } else {
+  return NextResponse.json(
+    { error: "Failed to update campaign" },
+    { status: 500 }
+  );
+  // }
 };
