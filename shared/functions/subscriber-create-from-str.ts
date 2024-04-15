40402758a14e5c621f6989aa7e8e035143cfcf01
nicholavila <nicholavila1@outlook.com>
2024-04-15 15:15:25 +0000

    update: subscriber create from string

---
+++ shared/functions/subscriber-create-from-str.ts
@@ -0,0 +1,54 @@
+import { createSubscriber } from "@/data/audience/subscriber-create";
+import { Subscriber } from "../types/subscriber";
+import { getSubscriberByEmail } from "@/data/audience/subscriber-by-email";
+import { checkValidationOfEmail } from "@/data/email/check-validation";
+
+type FunctionType = (
+  userEmail: string,
+  str: string,
+  splitter: string
+) => Promise<number>;
+
+export const createSubscriberFromStr: FunctionType = async (
+  userEmail,
+  str,
+  splitter
+) => {
+  const fields = str.split(splitter);
+  if (fields.length < 6) {
+    return 0;
+  }
+
+  const subscriberEmail = fields[0].trim();
+  const existingSubscriber = await getSubscriberByEmail({
+    userEmail,
+    subscriberEmail
+  });
+
+  if (existingSubscriber) {
+    return 0;
+  }
+
+  const valid = checkValidationOfEmail(subscriberEmail);
+  if (!valid) {
+    return 0;
+  }
+
+  const contact: Subscriber = {
+    userEmail,
+    subscriberEmail,
+    firstName: fields[1].trim(),
+    lastName: fields[2].trim(),
+    address: fields[3],
+    phoneNumber: fields[4],
+    birthday: new Date(fields[5]),
+    tags: [],
+    subscribed: true,
+    contactRating: 0,
+    created: new Date(),
+    lastChanged: new Date()
+  };
+
+  const res = await createSubscriber(contact);
+  return res.success ? 1 : 0;
+};
