b941bebe738799bbb38637351ee55211b61273d4
nicholavila <nicholavila1@outlook.com>
2024-04-16 09:24:09 +0000

    feat: validate api

---
+++ app/api/subscriber/validate/route.ts
@@ -0,0 +1,35 @@
+import { getSubscriberByEmail } from "@/data/audience/subscriber-by-email";
+import { updateSubscriber } from "@/data/audience/subscriber-update";
+import { Subscriber } from "@/shared/types/subscriber";
+import { NextRequest, NextResponse } from "next/server";
+
+// ## Validate [subscriberEmail]
+
+export const POST = async (request: NextRequest) => {
+  const data = await request.json();
+  const { ownerEmail, subscriberEmail, otp } = data;
+
+  const existingSubscriber: Subscriber = (await getSubscriberByEmail({
+    userEmail: ownerEmail,
+    subscriberEmail
+  })) as Subscriber;
+
+  if (!existingSubscriber) {
+    return NextResponse.json({}, { status: 404 });
+  }
+
+  if (existingSubscriber.otp !== otp) {
+    return NextResponse.json({ error: "OTP is invalid" });
+  }
+
+  const res_update = await updateSubscriber({
+    id: existingSubscriber.id as string,
+    validated: true
+  });
+
+  if (res_update.error) {
+    return NextResponse.json({}, { status: 404 });
+  }
+
+  return NextResponse.json({ validated: true });
+};
