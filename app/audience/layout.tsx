ce3a6c2d3e210cf6e05c8ea9a6bf59b39cac9644
nicholavila <nicholavila1@outlook.com>
2024-04-17 08:23:39 +0000

    update: set permission for audience page

--- app/audience/layout.tsx
+++ app/audience/layout.tsx
@@ -1,8 +1,20 @@
 "use client";
 
+import { currentUser } from "@/lib/auth";
+
 const AudienceLayout = ({
   children
 }: Readonly<{ children: React.ReactNode }>) => {
+  const user = currentUser();
+
+  if (!user) {
+    return (
+      <div>
+        <h1>Not logged in</h1>
+      </div>
+    );
+  }
+
   return children;
 };
 
