1aebb8408e89041a65601a3f3a063fc4eec5253a
nicholavila <nicholavila1@outlook.com>
2024-04-17 15:52:31 +0000

    refactor: subscriber by email

--- data/audience/subscriber-by-email.ts
+++ data/audience/subscriber-by-email.ts
@@ -1,14 +1,31 @@
-// import db from "@/lib/db";
-// import { GetCommand } from "@aws-sdk/lib-dynamodb";
+"use server";
 
 import { prisma } from "@/lib/prisma";
-
-// const TableName = process.env.AWS_DYNAMODB_MAILING_LIST_TABLE_NAME;
 
 type Params = {
   userEmail: string;
   subscriberEmail: string;
 };
+
+export const getSubscriberByEmail = async ({
+  userEmail,
+  subscriberEmail
+}: Params) => {
+  try {
+    return await prisma.mailinglist.findFirst({
+      where: {
+        userEmail: userEmail,
+        subscriberEmail: subscriberEmail
+      }
+    });
+  } catch (error) {
+    return null;
+  }
+};
+
+// import db from "@/lib/db";
+// import { GetCommand } from "@aws-sdk/lib-dynamodb";
+// const TableName = process.env.AWS_DYNAMODB_MAILING_LIST_TABLE_NAME;
 
 // export const getSubscriberByEmail = async ({
 //   userEmail,
@@ -26,19 +43,3 @@ type Params = {
 //     return null;
 //   }
 // };
-
-export const getSubscriberByEmail = async ({
-  userEmail,
-  subscriberEmail
-}: Params) => {
-  try {
-    return await prisma.mailinglist.findFirst({
-      where: {
-        userEmail: userEmail,
-        subscriberEmail: subscriberEmail
-      }
-    });
-  } catch (error) {
-    return null;
-  }
-};
