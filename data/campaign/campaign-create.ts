1f73c6b32b90f5c4697d8a44a84215eb16a91dac
nicholavila <nicholavila1@outlook.com>
2024-04-17 15:36:45 +0000

    chore: clean up

--- data/campaign/campaign-create.ts
+++ data/campaign/campaign-create.ts
@@ -1,16 +1,29 @@
 "use server";
 
-// import db from "@/lib/db";
-// import { PutCommand } from "@aws-sdk/lib-dynamodb";
 import { prisma } from "@/lib/prisma";
-
-// const TableName = process.env.AWS_DYNAMODB_CAMPAIGNS_TABLE_NAME;
 
 type Params = {
   userEmail: string;
   campaignId: string;
   title: string;
 };
+
+export const createCampaign = async (data: Params) => {
+  try {
+    await prisma.campaigns.create({
+      data: {
+        ...data
+      }
+    });
+    return { success: true };
+  } catch (error) {
+    return { error: true };
+  }
+};
+
+// import db from "@/lib/db";
+// import { PutCommand } from "@aws-sdk/lib-dynamodb";
+// const TableName = process.env.AWS_DYNAMODB_CAMPAIGNS_TABLE_NAME;
 
 // export const createCampaign = async (data: Params) => {
 //   const command = new PutCommand({
@@ -27,16 +40,3 @@ type Params = {
 //     return { error };
 //   }
 // };
-
-export const createCampaign = async (data: Params) => {
-  try {
-    await prisma.campaigns.create({
-      data: {
-        ...data
-      }
-    });
-    return { success: true };
-  } catch (error) {
-    return { error: true };
-  }
-};
