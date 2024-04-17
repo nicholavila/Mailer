39c34bb7d5d8b49b4014de618cab88a7386e462d
nicholavila <nicholavila1@outlook.com>
2024-04-17 15:04:01 +0000

    chore: clean up

--- data/campaign/campaign-by-id.ts
+++ data/campaign/campaign-by-id.ts
@@ -1,8 +1,25 @@
 "use server";
 
-// import db from "@/lib/db";
 import { prisma } from "@/lib/prisma";
 import { Campaign } from "@/shared/types/campaign";
+
+export const getCampaignById = async (campaignId: string) => {
+  try {
+    const campaign: Campaign = (await prisma.campaigns.findUnique({
+      where: {
+        campaignId
+      }
+    })) as unknown as Campaign;
+    if (campaign?.time?.date) {
+      campaign.time.date = new Date(campaign.time.date);
+    }
+    return campaign;
+  } catch (error) {
+    return null;
+  }
+};
+
+// import db from "@/lib/db";
 // import { GetCommand } from "@aws-sdk/lib-dynamodb";
 
 // const TableName = process.env.AWS_DYNAMODB_CAMPAIGNS_TABLE_NAME;
@@ -23,19 +40,3 @@ import { Campaign } from "@/shared/types/campaign";
 //     return null;
 //   }
 // };
-
-export const getCampaignById = async (campaignId: string) => {
-  try {
-    const campaign: Campaign = (await prisma.campaigns.findUnique({
-      where: {
-        campaignId
-      }
-    })) as unknown as Campaign;
-    if (campaign?.time?.date) {
-      campaign.time.date = new Date(campaign.time.date);
-    }
-    return campaign;
-  } catch (error) {
-    return null;
-  }
-};
