052428b7cf27b96f97d0c002bd077da2adc91758
nicholavila <nicholavila1@outlook.com>
2024-04-18 14:15:20 +0000

    update: navigation bar of app

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -4,89 +4,60 @@ import {
   NavigationMenu,
   NavigationMenuContent,
   NavigationMenuItem,
+  NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle
 } from "@/components/ui/navigation-menu";
 import React from "react";
 import { ListItem } from "@/components/utils/list-item";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+type SubItem = {
+  title: string;
+  href: string;
+  description: string;
+};
 
 type MenuItem = {
   title: string;
-  href: string;
-  children?: { title: string; href: string; description: string }[];
+  href?: string;
+  children?: SubItem[];
 };
 
 export const Navbar = () => {
-  const audienceItems: { title: string; href: string; description: string }[] =
-    [
-      {
-        title: "Audience Dashboard",
-        href: "/audience",
-        description:
-          "Your audience dashboard is the home for all your contacts and audiences"
-      },
-      {
-        title: "All contacts",
-        href: "/audience/contacts",
-        description: "All your contacts stored in your customer database"
-      },
-      {
-        title: "Segments",
-        href: "/audience/segments",
-        description: "Classify and manage your audience in segments"
-      }
-    ];
-
-  const campaignItems: { title: string; href: string; description: string }[] =
-    [
-      {
-        title: "All Campaigns",
-        href: "/campaign",
-        description: "All your campaigns stored in your campaign database"
-      },
-      {
-        title: "New Campaign",
-        href: "/campaign/create-campaign",
-        description: "Create a new email campaign"
-      }
-    ];
-
+  const path = usePathname();
   const menuItems: MenuItem[] = [
-    // {
-    //   title: "Features",
-    //   href: "/"
-    // },
-    // {
-    //   title: "Pricing",
-    //   href: "/"
-    // },
-    // {
-    //   title: "Resources",
-    //   href: "/"
-    // },
     {
-      title: "Campaign",
-      href: "/campaign",
-      children: campaignItems
+      title: "Audience Dashboard",
+      href: "/audience"
     },
     {
-      title: "Audience",
-      href: "/audience",
-      children: audienceItems
+      title: "All contacts",
+      href: "/audience/contacts"
+    },
+    {
+      title: "Segments",
+      href: "/audience/segments"
+    },
+    {
+      title: "All Campaigns",
+      href: "/campaign"
+    },
+    {
+      title: "New Campaign",
+      href: "/campaign/create-campaign"
     }
   ];
 
   return (
     <NavigationMenu>
       <NavigationMenuList className="gap-x-2">
-        {menuItems.map((menuItem) => (
-          <NavigationMenuItem key={menuItem.title}>
-            <NavigationMenuTrigger>
-              {/* <Link href={menuItem.href} passHref> */}
-              {menuItem.title}
-              {/* </Link> */}
-            </NavigationMenuTrigger>
-            {menuItem.children && (
+        {menuItems.map((menuItem) =>
+          menuItem.children ? (
+            <NavigationMenuItem key={menuItem.href}>
+              <NavigationMenuTrigger>{menuItem.title}</NavigationMenuTrigger>
               <NavigationMenuContent>
                 <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
                   {menuItem.children.map((item) => (
