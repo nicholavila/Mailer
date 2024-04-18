34d1b00afc75e1a549cb58c3349f63f44721c673
nicholavila <nicholavila1@outlook.com>
2024-04-18 13:00:57 +0000

    chore: clean up

--- data/campaign/campaign-delete.ts
+++ data/campaign/campaign-delete.ts
@@ -14,22 +14,3 @@ export const deleteCampaign = async (campaignId: string) => {
     return { error: true };
   }
 };
-
-// import db from "@/lib/db";
-// import { DeleteCommand } from "@aws-sdk/lib-dynamodb";
-
-// const TableName = process.env.AWS_DYNAMODB_CAMPAIGNS_TABLE_NAME;
-
-// export const deleteCampaign = async (userEmail: string, campaignId: string) => {
-//   const command = new DeleteCommand({
-//     TableName,
-//     Key: { userEmail, campaignId }
-//   });
-
-//   try {
-//     const response = await db.send(command);
-//     return { success: true, response };
-//   } catch (error) {
-//     return { error };
-//   }
-// };
