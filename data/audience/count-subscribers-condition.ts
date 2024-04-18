305b049b9e2fdd85b846633dfbc000d5d00bf473
nicholavila <nicholavila1@outlook.com>
2024-04-18 14:37:22 +0000

    update: param type

--- data/audience/count-subscribers-condition.ts
+++ data/audience/count-subscribers-condition.ts
@@ -2,10 +2,11 @@
 
 import { prisma } from "@/lib/prisma";
 
-export const getNubmersOfSubscribersByCondition = async (condition: any) => {
+export const getNumbersOfSubscribersByCondition = async (condition?: any) => {
   try {
     return await prisma.mailinglist.count(condition);
   } catch (error) {
+    console.error(error);
     return null;
   }
 };
