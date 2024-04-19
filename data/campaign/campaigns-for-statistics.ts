c45771af2711840789365c9b30d7fe2bbb11ffa1
nicholavila <nicholavila1@outlook.com>
2024-04-19 11:04:54 +0000

    feat: campagins for statistics

---
+++ data/campaign/campaigns-for-statistics.ts
@@ -0,0 +1,34 @@
+"use server";
+
+import { prisma } from "@/lib/prisma";
+import { getFirstDateOfMonthsAgo } from "@/shared/functions/get-date-months-ago";
+
+export const getAllCampaignsForStatistics = async (userEmail: string) => {
+  try {
+    const firstDate5MonthsAgo = getFirstDateOfMonthsAgo(5);
+    return await prisma.campaigns.findMany({
+      where: {
+        AND: [
+          { userEmail },
+          {
+            lastUpdated: {
+              gte: firstDate5MonthsAgo
+            }
+          },
+          {
+            OR: [{ state: "sending" }, { state: "sent" }]
+          }
+        ]
+      },
+      select: {
+        to: true,
+        openedNumber: true,
+        unsubedNumber: true,
+        bouncedNumber: true
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
