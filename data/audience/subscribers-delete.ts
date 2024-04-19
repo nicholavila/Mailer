cfa1f02581e1403216984749c64cc58c63f92bcf
nicholavila <nicholavila1@outlook.com>
2024-04-19 11:53:33 +0000

    chore: clean up

--- data/audience/subscribers-delete.ts
+++ data/audience/subscribers-delete.ts
@@ -20,28 +20,3 @@ export const deleteSubscribers = async (
     return { error: true };
   }
 };
-
-// import db from "@/lib/db";
-// import { DeleteCommand } from "@aws-sdk/lib-dynamodb";
-
-// const TableName = process.env.AWS_DYNAMODB_MAILING_LIST_TABLE_NAME;
-
-// export const deleteSubscribers = async (
-//   userEmail: string,
-//   subscriberEmails: string[]
-// ) => {
-//   try {
-//     const response = await Promise.all(
-//       subscriberEmails.map(async (subscriberEmail) => {
-//         const command = new DeleteCommand({
-//           TableName,
-//           Key: { userEmail, subscriberEmail }
-//         });
-//         await db.send(command);
-//       })
-//     );
-//     return { success: true };
-//   } catch (error) {
-//     return { error: true };
-//   }
-// };
