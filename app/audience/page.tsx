2cf9357d93fb245dbe78d75c584ebe80a32985a2
nicholavila <nicholavila1@outlook.com>
2024-04-19 13:50:39 +0000

    update: audience page

--- app/audience/page.tsx
+++ app/audience/page.tsx
@@ -3,6 +3,11 @@
 import { Badge } from "@/components/ui/badge";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import AreaChartPlot from "@/components/utils/areachart-plot";
+import { getNumbersOfSubscribersByCondition } from "@/data/audience/count-subscribers-condition";
+import { getAllCampaignsForStatistics } from "@/data/campaign/campaigns-for-statistics";
+import { useCurrentUser } from "@/hooks/use-current-user";
+import { getFirstDateOfMonthsAgo } from "@/shared/functions/get-date-months-ago";
+import { useEffect, useMemo, useState } from "react";
 import { FaArrowUp } from "react-icons/fa";
 
 const Blue = ({ children }: { children: React.ReactNode }) => {
@@ -14,6 +19,24 @@ const Strong = ({ children }: { children: React.ReactNode }) => {
 };
 
 const AudiencePage = () => {
+  const user = useCurrentUser();
+
+  const [totalNumber, setTotalNumber] = useState<number>(0);
+  const [lastNumber, setLastNumber] = useState<number>(0);
+  const [validatedNumber, setValidatedNumber] = useState<number>(0);
+
+  useEffect(() => {
+    // getNumbersOfSubscribersByCondition().then((numbers) => {
+    //   setTotalNumber(numbers || 0);
+    // });
+    // getNumbersOf4WeeksAgo().then((numbers) => {
+    //   setLastNumber(numbers || 0);
+    // });
+    // getNumbersOfValidated().then((numbers) => {
+    //   setValidatedNumber(numbers || 0);
+    // });
+  }, []);
+
   return (
     <main className="w-5/6 flex flex-col py-6">
       <p className="text-5xl text-green-700 font-semibold pb-6">
