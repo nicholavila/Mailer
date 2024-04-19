ae8fe9464ff4fc5f4d0dfadf7379a091a4dda733
nicholavila <nicholavila1@outlook.com>
2024-04-19 09:26:50 +0000

    chore: clean up

--- data/audience/subscriber-delete.ts
+++ data/audience/subscriber-delete.ts
@@ -2,11 +2,7 @@
 
 import { prisma } from "@/lib/prisma";
 
-export const deleteSubscriber = async (
-  // userEmail: string,
-  // subscriberEmail: string,
-  id: string
-) => {
+export const deleteSubscriber = async (id: string) => {
   try {
     await prisma.mailinglist.delete({
       where: {
@@ -18,25 +14,3 @@ export const deleteSubscriber = async (
     return { error: true };
   }
 };
-
-// import db from "@/lib/db";
-// import { DeleteCommand } from "@aws-sdk/lib-dynamodb";
-
-// const TableName = process.env.AWS_DYNAMODB_MAILING_LIST_TABLE_NAME;
-
-// export const deleteSubscriber = async (
-//   userEmail: string,
-//   subscriberEmail: string
-// ) => {
-//   const command = new DeleteCommand({
-//     TableName,
-//     Key: { userEmail, subscriberEmail }
-//   });
-
-//   try {
-//     const response = await db.send(command);
-//     return { success: true, response };
-//   } catch (error) {
-//     return { error };
-//   }
-// };
