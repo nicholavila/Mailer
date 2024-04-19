9ea3b61cf60e62cea2717f3bb838343994196597
nicholavila <nicholavila1@outlook.com>
2024-04-19 08:55:08 +0000

    chore: clean up

--- data/audience/subscriber-create.ts
+++ data/audience/subscriber-create.ts
@@ -15,24 +15,3 @@ export const createSubscriber = async (data: Subscriber) => {
     return { error: true };
   }
 };
-
-// import db from "@/lib/db";
-// import { PutCommand } from "@aws-sdk/lib-dynamodb";
-
-// const TableName = process.env.AWS_DYNAMODB_MAILING_LIST_TABLE_NAME;
-
-// export const createSubscriber = async (data: Subscriber) => {
-//   const command = new PutCommand({
-//     TableName,
-//     Item: {
-//       ...data
-//     }
-//   });
-
-//   try {
-//     const response = await db.send(command);
-//     return { success: true, response };
-//   } catch (error) {
-//     return { error };
-//   }
-// };
