0ffad80a342605b7f8a0fdd069853868ef72aeae
nicholavila <nicholavila1@outlook.com>
2024-04-19 10:33:03 +0000

    chore: clean up

--- data/audience/subscribers-all.ts
+++ data/audience/subscribers-all.ts
@@ -13,25 +13,3 @@ export const getAllSubscribersByEmail = async (userEmail: string) => {
     return null;
   }
 };
-
-// import db from "@/lib/db";
-// import { QueryCommand } from "@aws-sdk/lib-dynamodb";
-
-// const TableName = process.env.AWS_DYNAMODB_MAILING_LIST_TABLE_NAME;
-
-// export const getAllSubscribersByEmail = async (userEmail: string) => {
-//   const command = new QueryCommand({
-//     TableName,
-//     KeyConditionExpression: "userEmail = :userEmail",
-//     ExpressionAttributeValues: {
-//       ":userEmail": userEmail
-//     }
-//   });
-
-//   try {
-//     const response = await db.send(command);
-//     return response.Items;
-//   } catch (error) {
-//     return null;
-//   }
-// };
