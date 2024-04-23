deea265197974a1c7f83eee854efdd21cfce116e
nicholavila <nicholavila1@outlook.com>
2024-04-23 09:19:27 +0000

    chore: clean up

--- data/campaign/campaign-by-id.ts
+++ data/campaign/campaign-by-id.ts
@@ -18,25 +18,3 @@ export const getCampaignById = async (campaignId: string) => {
     return null;
   }
 };
-
-// import db from "@/lib/db";
-// import { GetCommand } from "@aws-sdk/lib-dynamodb";
-
-// const TableName = process.env.AWS_DYNAMODB_CAMPAIGNS_TABLE_NAME;
-
-// export const getCampaignById = async (
-//   userEmail: string,
-//   campaignId: string
-// ) => {
-//   const command = new GetCommand({
-//     TableName,
-//     Key: { userEmail, campaignId }
-//   });
-
-//   try {
-//     const response = await db.send(command);
-//     return response.Item;
-//   } catch (error) {
-//     return null;
-//   }
-// };
