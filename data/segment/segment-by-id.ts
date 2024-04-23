ccd4d1b4294b568d601a9697e4dcd45e54eafccf
nicholavila <nicholavila1@outlook.com>
2024-04-23 13:34:17 +0000

    chore: clean up

--- data/segment/segment-by-id.ts
+++ data/segment/segment-by-id.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import db from "@/lib/dynamo";
+import dynamoDB from "@/lib/dynamo";
 import { GetCommand } from "@aws-sdk/lib-dynamodb";
 
 const TableName = process.env.AWS_DYNAMODB_SEGMENTS_TABLE_NAME;
@@ -12,7 +12,7 @@ export const getSegmentById = async (userEmail: string, segmentId: string) => {
   });
 
   try {
-    const response = await db.send(command);
+    const response = await dynamoDB.send(command);
     return response.Item;
   } catch (error) {
     return null;
