c9625a07e81b12eb954bd1047c701c7cdb837bde
nicholavila <nicholavila1@outlook.com>
2024-04-29 09:30:57 +0000

    fix: typo

--- actions/auth/new-password.ts
+++ actions/auth/new-password.ts
@@ -39,13 +39,13 @@ export const newPassword = async (
 
   const { password } = validatedFields.data;
   const hashedPassword = await hash(password, 10);
-  const respone = await updateUserPassword({
+  const response = await updateUserPassword({
     email: userEmail,
     password: hashedPassword,
     emailVerified: new Date()
   });
 
-  if (respone.error) {
+  if (response.error) {
     return { error: "Server error!" };
   }
 
