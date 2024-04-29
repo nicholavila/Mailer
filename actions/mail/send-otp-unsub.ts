2000494cae8c9e7217bc4502c5cb24e1f2776cdf
nicholavila <nicholavila1@outlook.com>
2024-04-29 11:08:14 +0000

    fix: typo

--- actions/mail/send-otp-unsub.ts
+++ actions/mail/send-otp-unsub.ts
@@ -4,11 +4,11 @@ import { sendEmail } from "@/data/email/send-email";
 
 const fromEmail = process.env.SITE_OWNER_EMAIL as string;
 
-export const sendOtpUnsubEmail = async (email: string, otp: string) => {
+export const sendOtpUnSubEmail = async (email: string, otp: string) => {
   return await sendEmail({
     from: fromEmail,
     to: [email],
     subject: "Confirm your OTP",
-    html: `<p>Your OTP for unsubscription is ${otp}</p>`
+    html: `<p>Your OTP for unSubscription is ${otp}</p>`
   });
 };
